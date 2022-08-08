61a0c56da158e25f30199b3b8375fc05fa1ddecf
eb0687 <ebrahim0687@gmail.com>
2022-08-08 07:59:02 +0000

    added title to hop

--- lua/eb/plugin-settings/hop.lua
+++ lua/eb/plugin-settings/hop.lua
@@ -1,3 +1,10 @@
+--      __
+--     / /_  ____  ____
+--    / __ \/ __ \/ __ \
+--   / / / / /_/ / /_/ /
+--  /_/ /_/\____/ .___/
+--             /_/
+
 require('hop').setup({
     keys = 'etovxqpdygfblzhckisuran',
     jump_on_sole_occurrence = true,
