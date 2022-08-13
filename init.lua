73fda629a7635322dcd0aa21dd9dfb244249a6ca
eb0687 <ebrahim0687@gmail.com>
2022-08-13 11:54:34 +0300

    syntax formatter test

--- init.lua
+++ init.lua
@@ -5,9 +5,9 @@
 -- |_|_| |_|_|\__(_)_|\__,_|\__,_|
 
 -- main
-    require("eb.options")
-    require("eb.keymaps")
-    require("eb.plugins")
-    require("eb.colorscheme")
-    require("eb.plugin-settings")
-    require("eb.lsp")
+require("eb.options")
+require("eb.keymaps")
+require("eb.plugins")
+require("eb.colorscheme")
+require("eb.plugin-settings")
+require("eb.lsp")
