fa8c137c052968a0eb79bafa94c27753d0b26f0e
eb0687 <ebrahim0687@gmail.com>
2022-12-28 13:05:04 +0300

    added virtual text on hover instead of inline

--- lua/eb/lsp/configs.lua
+++ lua/eb/lsp/configs.lua
@@ -76,3 +76,11 @@ require 'lspconfig'.pylsp.setup {
 require 'lspconfig'.ansiblels.setup {
     capabilities = capabilities
 }
+
+vim.diagnostic.config({
+  virtual_text = false
+})
+
+-- Show line diagnostics automatically in hover window
+vim.o.updatetime = 250
+vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
