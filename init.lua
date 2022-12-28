ce6ec91c578ea47722999eb78b3eb2d91df28b56
eb0687 <ebrahim0687@gmail.com>
2022-12-29 00:21:48 +0300

    Refactor: re-organizing code

--- init.lua
+++ init.lua
@@ -5,9 +5,12 @@
 -- |_|_| |_|_|\__(_)_|\__,_|\__,_|
 
 -- main
-require("eb.options")
-require("eb.keymaps")
-require("eb.plugins")
-require("eb.colorscheme")
-require("eb.plugin-settings")
-require("eb.lsp")
+-- require("eb.options")
+-- require("eb.keymaps")
+-- require("eb.plugins")
+-- require("eb.colorscheme")
+-- require("eb.plugin-settings")
+-- require("eb.lsp")
+
+require("eb")
+print("Hello from ROOT")
