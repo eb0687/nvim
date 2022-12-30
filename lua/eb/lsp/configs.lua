677f034d5c88dadf913f84667689c534b2aff7ef
eb0687 <ebrahim0687@gmail.com>
2022-12-30 22:58:05 +0300

    Refactor: better keymap function for my lsp

--- lua/eb/lsp/configs.lua
+++ lua/eb/lsp/configs.lua
@@ -3,30 +3,37 @@ local nvim_lsp = require('lspconfig')
 -- Use an on_attach function to only map the following keys after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
 
-    -- Mappings
-    local function keymaps(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+    local keymap = function(keys, func, desc)
+        if desc then
+            desc = 'LSP: ' .. desc
+        end
 
-    local opts = { noremap = true, silent = true }
+        vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+    end
 
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    keymaps('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    keymaps('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    keymaps('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    keymaps('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-    keymaps('n', '<C-p>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-    keymaps('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-    -- keymaps('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-    keymaps('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-    keymaps('n', '<space>N', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-    keymaps('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    keymaps('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-    keymaps('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-    -- keymaps('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-    keymaps('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-    keymaps('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-    keymaps('n', '<space>gq', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-    keymaps('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-    keymaps('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+    keymap('rn', vim.lsp.buf.rename, '[R]e[N]ame')
+    keymap('ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+
+    keymap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+    keymap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+    keymap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+    keymap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+    keymap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+    keymap('<space>f', '<cmd>lua vim.lsp.buf.format()<CR>', '[F]ormat code')
+    keymap('df', vim.diagnostic.open_float, 'Open [D]iagnostic [F]loat')
+
+    -- See `:help K` for why this keymap
+    keymap('K', vim.lsp.buf.hover, 'Hover Documentation')
+    keymap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+
+    -- Lesser used LSP functionality
+    -- keymap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+    -- keymap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+    -- keymap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+    -- keymap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+    -- keymap('<leader>wl', function()
+    --     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    -- end, '[W]orkspace [L]ist Folders')
 
     -- Disable Autoformat
     client.server_capabilities.document_formatting = false
@@ -78,7 +85,7 @@ require 'lspconfig'.ansiblels.setup {
 }
 
 vim.diagnostic.config({
-  virtual_text = false
+    virtual_text = false
 })
 
 -- Show line diagnostics automatically in hover window
