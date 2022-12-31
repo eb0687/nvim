aa70310ba48cd09bed8dc26598e29e16d1d271e6
eb0687 <ebrahim0687@gmail.com>
2022-12-31 12:38:09 +0300

    Refactor: moved OSC52 configs to after directory

--- after/plugin/osc52.lua
+++ after/plugin/osc52.lua
@@ -1,5 +1,16 @@
 -- OSC52
 
+-- TODO: create a pcall for this plugin
+
+-- SETUP [[[
+
+require('osc52').setup {
+  max_length = 0,  -- Maximum length of selection (0 for no limit)
+  silent = false,  -- Disable message on successful copy
+  trim = false,    -- Trim text before copy
+}
+
+-- ]]]
 -- KEYMAPS [[[
 
 -- Variables
