7c0396010d45da7395b66216921b9333491d1e10
eb0687 <ebrahim0687@gmail.com>
2022-12-31 13:13:22 +0300

    Refactor: Moved CMP configs to after/plugin directory. this will load only after plugin has been loaded.

--- lua/eb/plugin-settings/init.lua
+++ lua/eb/plugin-settings/init.lua
@@ -3,9 +3,7 @@
 -- nvim plugin settings
 require("eb.plugin-settings.indent-blankline")
 require("eb.plugin-settings.lualine")
--- require("eb.plugin-settings.autopair")
 require("eb.plugin-settings.treesitter")
-require("eb.plugin-settings.cmp")
 require("eb.plugin-settings.gitsigns")
 require("eb.plugin-settings.nvim-surround")
 require("eb.plugin-settings.colorizer")
