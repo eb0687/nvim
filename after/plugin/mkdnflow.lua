be19df978eaff99453941927c9ee91e1c68fff2e
eb0687 <ebrahim0687@gmail.com>
2023-01-13 00:09:44 +0300

    Cleanup: comment cleanup

--- after/plugin/mkdnflow.lua
+++ after/plugin/mkdnflow.lua
@@ -6,6 +6,7 @@ require('mkdnflow').setup({
     modules = {
         -- NOTE: disable all keybindings
         -- this is a temporary workaround to allow normal buffer navigation using TAB and SHIFT+TAB
+        -- if you want to re-enable bindings change maps to true
         -- refer to this page for default configs -- https://github.com/jakewvincent/mkdnflow.nvim?utm_source=pocket_saves#%EF%B8%8F-configuration
         maps = false,
     },
