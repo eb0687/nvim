e2b4c4f614e0cb6a3116d19edee6371ca044cff9
eb0687 <ebrahim0687@gmail.com>
2023-07-07 12:46:32 +0300

    Feat: add keymap to clear quickfixlist
    add a keymap to clear the quickfixlist

--- lua/eb/keymaps.lua
+++ lua/eb/keymaps.lua
@@ -84,6 +84,7 @@ keymap_silent("n", "n", "nzz", 'Keep cursor centered while searching') -- naviga
 -- Quickfix list
 keymap_silent("n", "<leader>j", "<cmd>cnext<CR>zz", 'Quickfix next')
 keymap_silent("n", "<leader>k", "<cmd>cprev<CR>zz", 'Quickfix prev')
+keymap_silent("n", "<leader>cf", ":ClearQuickfixList<CR>", 'Clear the Quickfix')
 -- Various
 keymap_silent("n", "<leader>xx", "<cmd>silent !chmod +x %<CR>", 'Make executable')
 keymap_silent("n", "<leader>w", ":set wrap!<CR>", 'Toggle wrap')
