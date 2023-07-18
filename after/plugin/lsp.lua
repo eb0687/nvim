6ac8d59aa2950c508166e07f6bb2ab6f3737019f
eb0687 <ebrahim0687@gmail.com>
2023-07-18 11:45:44 +0300

    Chore: add format option local to LSP buffer

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -72,14 +72,10 @@ local on_attach = function(client, bufnr)
     keymap('K', vim.lsp.buf.hover, 'Hover Documentation')
     keymap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
 
-    -- Lesser used LSP functionality
-    -- keymap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-    -- keymap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-    -- keymap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-    -- keymap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-    -- keymap('<leader>wl', function()
-    --     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    -- end, '[W]orkspace [L]ist Folders')
+    -- Create a command `:Format` local to the LSP buffer
+    vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+        vim.lsp.buf.format()
+    end, { desc = 'Format current buffer with LSP' })
 
     -- Disable Autoformat
     client.server_capabilities.document_formatting = false
