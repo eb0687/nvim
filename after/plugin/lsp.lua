dea8d9550abbc4638261da07788c6a28b9d23956
eb0687 <ebrahim0687@gmail.com>
2023-07-18 11:52:11 +0300

    Feat: fix diagnostics window

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -166,15 +166,15 @@ vim.o.updatetime = 250
 -- NOTE: Show diagnostics in a pop-up window on hover
 -- SOURCE: https://www.reddit.com/r/neovim/comments/mqspfo/is_there_any_way_to_hide_the_diagnostic_from/
 _G.LspDiagnosticsPopupHandler = function()
-  local current_cursor = vim.api.nvim_win_get_cursor(0)
-  local last_popup_cursor = vim.w.lsp_diagnostics_last_cursor or {nil, nil}
+    local current_cursor = vim.api.nvim_win_get_cursor(0)
+    local last_popup_cursor = vim.w.lsp_diagnostics_last_cursor or { nil, nil }
 
-  -- Show the popup diagnostics window,
-  -- but only once for the current cursor location (unless moved afterwards).
-  if not (current_cursor[1] == last_popup_cursor[1] and current_cursor[2] == last_popup_cursor[2]) then
-    vim.w.lsp_diagnostics_last_cursor = current_cursor
-    vim.diagnostic.open_float(0, {scope="cursor"})   -- for neovim 0.6.0+, replaces show_{line,position}_diagnostics
-  end
+    -- Show the popup diagnostics window,
+    -- but only once for the current cursor location (unless moved afterwards).
+    if not (current_cursor[1] == last_popup_cursor[1] and current_cursor[2] == last_popup_cursor[2]) then
+        vim.w.lsp_diagnostics_last_cursor = current_cursor
+        vim.diagnostic.open_float(0, { scope = "cursor" }) -- for neovim 0.6.0+, replaces show_{line,position}_diagnostics
+    end
 end
 vim.cmd [[
 augroup LSPDiagnosticsOnHover
@@ -182,7 +182,6 @@ augroup LSPDiagnosticsOnHover
   autocmd CursorHold *   lua _G.LspDiagnosticsPopupHandler()
 augroup END
 ]]
-
 
 -- ]]]
 
