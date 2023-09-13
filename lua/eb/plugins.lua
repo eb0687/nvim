30c4c28738716e96e9dd1fe3b6f311ef0e259f08
eb0687 <ebrahim0687@gmail.com>
2023-09-13 19:59:34 +0300

    Feat: replace packer with lazy.nvim
    packer is no longer being maintained, lazy.nvim by folke seems like the
    next best alternative

--- lua/eb/plugins.lua
+++ lua/eb/plugins.lua
@@ -1,165 +1,132 @@
---    ___           _
---   / _ \__ _  ___| | _____ _ __
---  / /_)/ _` |/ __| |/ / _ \ '__|
--- / ___/ (_| | (__|   <  __/ |
--- \/    \__,_|\___|_|\_\___|_|
+-- lazy.nvim
+-- https://github.com/folke/lazy.nvim
 
--- [[[ Automatically install packer
-local fn = vim.fn
-
-local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-    PACKER_BOOTSTRAP = fn.system {
+-- Bootstrap
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
         "git",
         "clone",
-        "--depth",
-        "1",
-        "https://github.com/wbthomason/packer.nvim",
-        install_path,
-    }
-    print "Installing packer close and reopen Neovim..."
-    vim.cmd [[packadd packer.nvim]]
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
--- ]]]
+vim.opt.rtp:prepend(lazypath)
 
--- [[[ Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd [[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]]
--- ]]]
+-- Install plugins here
+local plugins = {
 
--- [[[ Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-    return
-end
---]]]
-
--- [[[ Have packer use a popup window
--- packer.init {
---   display = {
---     open_fn = function()
---       return require("packer.util").float { border = "rounded" }
---     end,
---   },
--- }
--- ]]]
-
--- [[[ Install plugins here
-return packer.startup(function(use)
     -- Required plugins
-    use "wbthomason/packer.nvim" -- Have packer manage itself
-    use "nvim-lua/popup.nvim"    -- An implementation of the Popup API from vim in Neovim
-    use "nvim-lua/plenary.nvim"  -- Useful lua functions used by lots of plugins
+    -- 'wbthomason/packer.nvim', -- Have packer manage itself
+    'nvim-lua/popup.nvim',    -- An implementation of the Popup API from vim in Neovim
+    'nvim-lua/plenary.nvim',  -- Useful lua functions used by lots of plugins
 
     -- Colorscheme & Aesthetics
-    use "sainnhe/gruvbox-material" -- gruvbox material colorscheme
-    use "romgrk/barbar.nvim"
-    use 'kyazdani42/nvim-web-devicons'
-    use 'nvim-lualine/lualine.nvim'
+    'sainnhe/gruvbox-material', -- gruvbox material colorscheme
+    'romgrk/barbar.nvim',
+    'kyazdani42/nvim-web-devicons',
+    'nvim-lualine/lualine.nvim',
 
     -- Utilities
-    use 'lukas-reineke/indent-blankline.nvim'
-    use "tenxsoydev/tabs-vs-spaces.nvim"      -- https://github.com/tenxsoydev/tabs-vs-spaces.nvim
-    use 'windwp/nvim-autopairs'
-    use 'ThePrimeagen/harpoon'                -- https://github.com/ThePrimeagen/harpoon
-    use 'kyazdani42/nvim-tree.lua'            -- https://github.com/kyazdani42/nvim-tree.lua
-    use 'farmergreg/vim-lastplace'            -- https://github.com/farmergreg/vim-lastplace
-    use 'tpope/vim-commentary'                -- https://github.com/tpope/vim-commentary
-    use 'kylechui/nvim-surround'              -- https://github.com/kylechui/nvim-surround
-    use { 'phaazon/hop.nvim', branch = 'v2' } -- https://github.com/phaazon/hop.nvim
-    use 'Pocco81/true-zen.nvim'               -- https://github.com/Pocco81/true-zen.nvim
-    use 'christoomey/vim-tmux-navigator'      -- better navigation between nvim splits and tmux panes -- https://github.com/christoomey/vim-tmux-navigator
-    use 'jvirtanen/vim-hcl'
-    use 'mboughaba/i3config.vim'
-    use { 'ojroques/nvim-osc52' }         -- copy text to the system clipboard using the ANSI OSC52 sequence -- https://github.com/ojroques/nvim-osc52
-    use { 'norcalli/nvim-colorizer.lua' } -- https://github.com/norcalli/nvim-colorizer.lua
-    use { 'folke/todo-comments.nvim' }
-    use { 'mbbill/undotree' }             -- https://github.com/mbbill/undotree
-    -- use {
-    --     'klesh/nvim-runscript',
-    --     config = function() require("nvim-runscript").setup{} end
-    -- } -- https://github.com/klesh/nvim-runscript
-    use { 'CRAG666/code_runner.nvim', requires = 'nvim-lua/plenary.nvim' }
-    use { 'michaelb/sniprun', run = 'sh ./install.sh' } -- NOTE: testing https://github.com/michaelb/sniprun
-    use { 'epwalsh/obsidian.nvim' }
+    'lukas-reineke/indent-blankline.nvim',
+    'tenxsoydev/tabs-vs-spaces.nvim', -- https://github.com/tenxsoydev/tabs-vs-spaces.nvim
+    'windwp/nvim-autopairs',
+    'ThePrimeagen/harpoon',           -- https://github.com/ThePrimeagen/harpoon
+    'kyazdani42/nvim-tree.lua',       -- https://github.com/kyazdani42/nvim-tree.lua
+    'farmergreg/vim-lastplace',       -- https://github.com/farmergreg/vim-lastplace
+    'tpope/vim-commentary',           -- https://github.com/tpope/vim-commentary
+    'kylechui/nvim-surround',         -- https://github.com/kylechui/nvim-surround
+    {
+        'phaazon/hop.nvim',
+        branch = 'v2'
+    },
+    'Pocco81/true-zen.nvim',          -- https://github.com/Pocco81/true-zen.nvim
+    'christoomey/vim-tmux-navigator', -- better navigation between nvim splits and tmux panes -- https://github.com/christoomey/vim-tmux-navigator
+    'jvirtanen/vim-hcl',
+    'mboughaba/i3config.vim',
+    'ojroques/nvim-osc52',         -- copy text to the system clipboard using the ANSI OSC52 sequence -- https://github.com/ojroques/nvim-osc52
+    'norcalli/nvim-colorizer.lua', -- https://github.com/norcalli/nvim-colorizer.lua
+    'folke/todo-comments.nvim',
+    'mbbill/undotree',             -- https://github.com/mbbill/undotree
+    {
+        'CRAG666/code_runner.nvim',
+        dependencies = 'nvim-lua/plenary.nvim'
+    },
+    {
+        'michaelb/sniprun',
+        build = 'sh ./install.sh'
+    },
+    'epwalsh/obsidian.nvim',
 
     -- Telescope
-    use 'nvim-telescope/telescope.nvim'
-    use 'nvim-telescope/telescope-github.nvim'
+    'nvim-telescope/telescope.nvim',
+    'nvim-telescope/telescope-github.nvim',
 
     -- Treesitter
-    -- Note: :TSUpdate will cause Packer to fail upon the first installation. It will run correctly when updating. To avoid this, call nvim-treesitter.install.update()
-    -- Source: https://github.com/nvim-treesitter/nvim-treesitter/wiki/Installation
-    use {
+    {
         'nvim-treesitter/nvim-treesitter',
-        run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
-    }
-    use({
-        "nvim-treesitter/nvim-treesitter-textobjects",
-        after = "nvim-treesitter",
-        requires = "nvim-treesitter/nvim-treesitter",
-    })
-    use 'HiPhish/rainbow-delimiters.nvim' -- https://gitlab.com/HiPhish/rainbow-delimiters.nvim
+        build = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+    },
+    {
+        'nvim-treesitter/nvim-treesitter-textobjects',
+        dependencies = 'nvim-treesitter/nvim-treesitter',
+    },
+    'HiPhish/rainbow-delimiters.nvim', -- https://gitlab.com/HiPhish/rainbow-delimiters.nvim
 
     -- Completions (cmp)
-    use 'hrsh7th/nvim-cmp'                    -- main completion plugin
-    use 'hrsh7th/cmp-buffer'                  -- plugin for buffer completion
-    use 'hrsh7th/cmp-path'                    -- plugin for path completion
-    use 'saadparwaiz1/cmp_luasnip'            -- plugin for snippet completion
-    use 'hrsh7th/cmp-cmdline'                 -- plugin for command line completion
-    use 'hrsh7th/cmp-nvim-lsp'                -- plugin for lsp completion
-    use 'hrsh7th/cmp-nvim-lua'                -- plugin for nvim-lua completion
-    use 'hrsh7th/cmp-nvim-lsp-signature-help' -- https://github.com/hrsh7th/cmp-nvim-lsp-signature-help
+    'hrsh7th/nvim-cmp',                    -- main completion plugin
+    'hrsh7th/cmp-buffer',                  -- plugin for buffer completion
+    'hrsh7th/cmp-path',                    -- plugin for path completion
+    'saadparwaiz1/cmp_luasnip',            -- plugin for snippet completion
+    'hrsh7th/cmp-cmdline',                 -- plugin for command line completion
+    'hrsh7th/cmp-nvim-lsp',                -- plugin for lsp completion
+    'hrsh7th/cmp-nvim-lua',                -- plugin for nvim-lua completion
+    'hrsh7th/cmp-nvim-lsp-signature-help', -- https://github.com/hrsh7th/cmp-nvim-lsp-signature-help
 
     -- Snippets
-    use 'L3MON4D3/LuaSnip'             -- https://github.com/L3MON4D3/LuaSnip
-    use 'rafamadriz/friendly-snippets' -- https://github.com/rafamadriz/friendly-snippets
+    'L3MON4D3/LuaSnip',             -- https://github.com/L3MON4D3/LuaSnip
+    'rafamadriz/friendly-snippets', -- https://github.com/rafamadriz/friendly-snippets
 
     -- LSP
-    use 'neovim/nvim-lspconfig'             -- plugin to enable LSP - https://github.com/neovim/nvim-lspconfig
-    use 'williamboman/mason.nvim'           -- plugin to auto download language servers - https://github.com/williamboman/mason.nvim
-    use 'williamboman/mason-lspconfig.nvim' -- plugin required for mason - https://github.com/williamboman/mason-lspconfig.nvim
+    'neovim/nvim-lspconfig',             -- plugin to enable LSP - https://github.com/neovim/nvim-lspconfig
+    'williamboman/mason.nvim',           -- plugin to auto download language servers - https://github.com/williamboman/mason.nvim
+    'williamboman/mason-lspconfig.nvim', -- plugin required for mason - https://github.com/williamboman/mason-lspconfig.nvim
 
     -- Null-Ls
-    use 'jose-elias-alvarez/null-ls.nvim' -- syntax formatter - https://github.com/jose-elias-alvarez/null-ls.nvim
+    'jose-elias-alvarez/null-ls.nvim', -- syntax formatter - https://github.com/jose-elias-alvarez/null-ls.nvim
 
     -- Git
-    use 'lewis6991/gitsigns.nvim' -- plugin for git integration
-    use 'ThePrimeagen/git-worktree.nvim'
-    use 'tpope/vim-fugitive'
+    'lewis6991/gitsigns.nvim', -- plugin for git integration
+    'ThePrimeagen/git-worktree.nvim',
+    'tpope/vim-fugitive',
 
     -- FZF
-    use 'ibhagwan/fzf-lua' -- https://github.com/ibhagwan/fzf-lua
+    'ibhagwan/fzf-lua', -- https://github.com/ibhagwan/fzf-lua
 
     -- Markdown
-    use({
+    {
         'jakewvincent/mkdnflow.nvim',
-        rocks = 'luautf8', -- Ensures optional luautf8 dependency is installed
-    })                     -- https://github.com/jakewvincent/mkdnflow.nvim
-
-    -- NOTE:: throws an error, disabling for now
-    -- use 'ekickx/clipboard-image.nvim' -- https://github.com/ekickx/clipboard-image.nvim
+        -- rocks = 'luautf8', -- Ensures optional luautf8 dependency is installed
+    },
 
     -- HTML & CSS
-    use({
-        "aurum77/live-server.nvim",
-        run = function()
+    {
+        'aurum77/live-server.nvim',
+        build = function()
             require "live_server.util".install()
         end,
-        cmd = { "LiveServer", "LiveServerStart", "LiveServerStop" },
-    })
+        cmd = {
+            'LiveServer',
+            'LiveServerStart',
+            'LiveServerStop' },
+    },
 
     -- Godot
-    use "lommix/godot.nvim"
+    'lommix/godot.nvim',
+}
 
-    -- Automatically set up your configuration after cloning packer.nvim
-    -- Put this at the end after all plugins
-    if PACKER_BOOTSTRAP then
-        require("packer").sync()
-    end
-end)
--- ]]]
+local opts = {}
+
+require("lazy").setup(plugins,opts)
