c76017931f17789306397ba12f15c4f61831cbf6
eb0687 <ebrahim0687@gmail.com>
2023-09-14 12:13:09 +0300

    Feat: add init.lua for loading plugins without a config

---
+++ lua/eb/plugins/init.lua
@@ -0,0 +1,7 @@
+-- Plugins that do not have any configuration are placed here
+
+return {
+    -- https://github.com/christoomey/vim-tmux-navigator
+    -- better navigation between nvim splits and tmux panes
+    'christoomey/vim-tmux-navigator',
+}
