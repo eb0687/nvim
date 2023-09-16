e38944c5bcfb10814fa50126f9cbf28a3729d4c1
eb0687 <ebrahim0687@gmail.com>
2023-09-16 12:47:29 +0300

    Fix: change plugin name

--- lua/eb/plugins/null-ls.lua
+++ lua/eb/plugins/null-ls.lua
@@ -8,7 +8,7 @@ return {
     'jose-elias-alvarez/null-ls.nvim',
 
     config = function()
-        local null_ls = require("module")
+        local null_ls = require("null-ls")
         local formatting = null_ls.builtins.formatting
         local diagnostics = null_ls.builtins.diagnostics
 
