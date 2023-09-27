5ace81036931cba06ad0a04d4743d6cc4508d9cc
eb0687 <ebrahim0687@gmail.com>
2023-09-27 10:29:51 +0300

    Fix: remove deprecated command

--- lua/eb/plugins/nvim-tree.lua
+++ lua/eb/plugins/nvim-tree.lua
@@ -39,7 +39,6 @@ return {
                 centralize_selection = true,
                 width = 40,
                 -- height = 30,
-                hide_root_folder = false,
                 side = "left",
                 preserve_window_proportions = false,
                 number = true,
