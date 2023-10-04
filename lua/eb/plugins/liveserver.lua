59654230a61cf17cb2c925c657b6a50606276c82
eb0687 <ebrahim0687@gmail.com>
2023-10-04 13:08:51 +0300

    Chore: add configs to live-server

--- lua/eb/plugins/liveserver.lua
+++ lua/eb/plugins/liveserver.lua
@@ -12,7 +12,12 @@ return {
     "aurum77/live-server.nvim",
     event = "VeryLazy",
     config = function()
-        require('live_server').setup {}
+        require('live_server').setup {
+            port = 8080,
+            browser_command = "", -- Empty string starts up with default browser
+            quiet = false,
+            no_css_inject = false, -- Disables css injection if true, might be useful when testing out tailwindcss
+        }
     end,
     cmd = {
         "LiveServer",
