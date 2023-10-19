e0b261870b8efd4c54a54644baa10e13bbd0d0e4
eb0687 <ebrahim0687@gmail.com>
2023-10-19 15:50:55 +0300

    feat(plugin): add tailwindcss-colorizer-cmp
    shows colors in completion window when working with tailwind projects

--- lua/eb/plugins/init.lua
+++ lua/eb/plugins/init.lua
@@ -85,4 +85,14 @@ return {
         },
         cmd = 'Nerdy',
     },
+    -- https://github.com/roobert/tailwindcss-colorizer-cmp.nvim
+    {
+        "roobert/tailwindcss-colorizer-cmp.nvim",
+        -- optionally, override the default options:
+        config = function()
+            require("tailwindcss-colorizer-cmp").setup({
+                color_square_width = 2,
+            })
+        end
+    }
 }
