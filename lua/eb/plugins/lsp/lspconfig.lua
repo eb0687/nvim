b8f2b67a1888c6f2ed791863320af933b13d4c86
eb0687 <ebrahim0687@gmail.com>
2023-10-26 14:53:51 +0300

    refactor(lsp): rearrange code

--- lua/eb/plugins/lsp/lspconfig.lua
+++ lua/eb/plugins/lsp/lspconfig.lua
@@ -1,4 +1,4 @@
---   _                            __ _
+--  _                            __ _
 -- | |___ _ __   ___ ___  _ __  / _(_) __ _
 -- | / __| '_ \ / __/ _ \| '_ \| |_| |/ _` |
 -- | \__ \ |_) | (_| (_) | | | |  _| | (_| |
@@ -13,7 +13,15 @@ return {
         local lspconfig = require("lspconfig")
         local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-        -- KEYMAPS
+        -- Capabilities
+        -- local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+        local capabilities = cmp_nvim_lsp.default_capabilities()
+
+        -- NOTE:
+        -- Capabilities required for the visualstudio lsps (css, html, etc)
+        -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#cssls
+        capabilities.textDocument.completion.completionItem.snippetSupport = true
+
         -- NOTE: Use an on_attach function to only map the following keys after the language server attaches to the current buffer
         local on_attach = function(client, bufnr)
             local keymap = function(keys, func, desc)
@@ -24,6 +32,7 @@ return {
                 vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
             end
 
+            -- KEYMAPS
             keymap("[d", ":Lspsaga diagnostic_jump_prev<CR>", "Go to previous diagnostic message")
             keymap("]d", ":Lspsaga diagnostic_jump_next<CR>", "Go to next diagnostic message")
             keymap("gp", ":Lspsaga peek_definition<CR>", "Go to next diagnostic message")
@@ -33,9 +42,10 @@ return {
             keymap("fi", ":Lspsaga finder<CR>", "Finder saga window")
             keymap("gd", require("telescope.builtin").lsp_definitions, "Goto Definition")
             keymap("gD", ":Lspsaga goto_definition<CR>", "Goto Definition")
+            keymap("df", ":Lspsaga show_cursor_diagnostics ++normal<CR>", "Open Diagnostic Float")
 
             keymap("gI", vim.lsp.buf.implementation, "Goto Implementation")
-            keymap("df", vim.diagnostic.open_float, "Open Diagnostic Float")
+            -- keymap("df", vim.diagnostic.open_float, "Open Diagnostic Float")
             -- keymap('rn', vim.lsp.buf.rename, 'ReName')
             -- keymap('<leader>ds', require('telescope.builtin').lsp_document_symbols, 'Document Symbols')
             -- keymap('gr', require('telescope.builtin').lsp_references, 'Goto References')
@@ -73,169 +83,142 @@ return {
             -- )
         end
 
-        -- NOTE: Language servers go here
-        local servers = {
-            "vimls",
-            -- NOTE: bashls integrates shellcheck by default
-            -- https://github.com/jose-elias-alvarez/null-ls.nvim/discussions/951
-            "bashls",
-            "pyright",
-            "lua_ls",
-            "ansiblels",
-            "jsonls",
-            "sqlls",
-            -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#emmet_language_server
-            -- https://github.com/olrtg/emmet-language-server
-            "emmet_language_server",
-            -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#eslint
-            -- https://github.com/hrsh7th/vscode-langservers-extracted
-            -- 'eslint',
-            -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#tsserver
-            -- https://github.com/typescript-language-server/typescript-language-server
-            "tsserver",
-            -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#cssls
-            -- 'cssls',
-            -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#tailwindcss
-            -- https://github.com/tailwindlabs/tailwindcss-intellisense
-            "tailwindcss",
+        -- LANGUAGE SERVER CONFIGURATION
+
+        -- vim
+        -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#vimls
+        -- https://github.com/iamcco/vim-language-server
+        lspconfig.vimls.setup {
+            on_attach = on_attach,
+            capabilities = capabilities,
         }
 
-        -- Capabilities
-        -- local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
-        local capabilities = cmp_nvim_lsp.default_capabilities()
+        -- ansible
+        -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#ansiblels
+        -- https://github.com/ansible/ansible-language-server
+        lspconfig.ansiblels.setup {
+            on_attach = on_attach,
+            capabilities = capabilities,
+        }
 
-        -- NOTE:
-        -- Capabilities required for the visualstudio lsps (css, html, etc)
-        -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#cssls
-        capabilities.textDocument.completion.completionItem.snippetSupport = true
-
-        for _, lsp in ipairs(servers) do
-            lspconfig[lsp].setup({
-                on_attach = on_attach,
-                capabilities = capabilities,
-                sqlls = {
-                    cmd = { "sql-language-server", "up", "--method", "stdio" },
-                    filetypes = { "sql", "mysql" },
-                    root_dir = function()
-                        return vim.loop.cwd()
-                    end,
-                },
-                settings = {
-                    Lua = {
-                        runtime = {
-                            version = "LuaJIT",
-                        },
-                        diagnostics = {
-                            -- NOTE: Get the language server to recognize the `vim` global
-                            globals = { "vim" },
-                        },
-                        telemetry = {
-                            enable = false,
-                        },
-                        completion = {
-                            callSnippet = "Replace",
-                        },
+        -- lua
+        -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#lua_ls
+        -- https://github.com/luals/lua-language-server
+        lspconfig.lua_ls.setup {
+            on_attach = on_attach,
+            capabilities = capabilities,
+            settings = { -- custom settings for lua
+                Lua = {
+                    runtime = {
+                        version = "LuaJIT",
+                    },
+                    telemetry = {
+                        enable = false,
+                    },
+                    -- make the language server recognize "vim" global
+                    diagnostics = {
+                        globals = { "vim" },
                     },
                 },
-            })
-        end
+            },
+        }
 
-        require("lspconfig").emmet_language_server.setup({
+        -- pyright
+        -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#pyright
+        -- https://github.com/microsoft/pyright
+        lspconfig.pyright.setup {
+            capabilities = capabilities,
+            on_attach = on_attach,
+            settings = {
+                pyright = {
+                    disableOrganizeImports = false,
+                    analysis = {
+                        useLibraryCodeForTypes = true,
+                        autoSearchPaths = true,
+                        diagnosticMode = "workspace",
+                        autoImportCompletions = true,
+                    },
+                },
+            },
+        }
+
+        -- javascript, typescript
+        -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#tsserver
+        -- https://github.com/typescript-language-server/typescript-language-server
+        lspconfig.tsserver.setup {
+            on_attach = on_attach,
             capabilities = capabilities,
             filetypes = {
-                "css",
-                "eruby",
-                "html",
                 "javascript",
                 "javascriptreact",
-                "less",
+                "javascript.jsx",
+                "typescript",
+                "typescriptreact",
+                "typescript.tsx"
+            },
+            root_dir = lspconfig.util.root_pattern("package.json", "tsconfig.json", ".git"),
+        }
+
+        -- bash
+        -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#bashls
+        -- https://github.com/bash-lsp/bash-language-server
+        -- NOTE: bashls integrates shellcheck by default
+
+        lspconfig.bashls.setup {
+            on_attach = on_attach,
+            capabilities = capabilities,
+            filetypes = { "sh" },
+        }
+
+        -- html, typescriptreact, javascriptreact, css, sass, scss, less, svelte, vue
+        -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#emmet_ls
+        -- https://github.com/aca/emmet-ls
+        lspconfig.emmet_ls.setup {
+            capabilities = capabilities,
+            on_attach = on_attach,
+            filetypes = {
+                "html",
+                "typescriptreact",
+                "javascriptreact",
+                "javascript",
+                "css",
                 "sass",
                 "scss",
+                "less",
                 "svelte",
-                "pug",
-                "typescriptreact",
                 "vue",
             },
+        }
 
-            -- https://github.com/olrtg/emmet-language-server#neovim
-            init_options = {
-                showSuggestionsAsSnippets = true,
-            },
-        })
+        -- json
+        lspconfig.jsonls.setup {
+            capabilities = capabilities,
+            on_attach = on_attach,
+            filetypes = { "json", "jsonc" },
+        }
 
-        -- require 'lspconfig'.bashls.setup {
-        --     capabilities = capabilities
-        -- }
+        -- tailwindcss
+        -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#tailwindcss
+        -- https://github.com/tailwindlabs/tailwindcss-intellisense
+        -- NOTE: this requires tailwind to be setup properly in the poject directory
+        lspconfig.tailwindcss.setup {
+            capabilities = capabilities,
+            on_attach = on_attach,
 
-        -- require 'lspconfig'.vimls.setup {
-        --     capabilities = capabilities
-        -- }
+        }
 
-        -- require 'lspconfig'.pylsp.setup {
-        --     capabilities = capabilities
-        -- }
-
-        -- require 'lspconfig'.ansiblels.setup {
-        --     capabilities = capabilities
-        -- }
-
-        -- require 'lspconfig'.jsonls.setup {
-        --     capabilities = capabilities
-        -- }
-
-        -- require 'lspconfig'.terraformls.setup {
-        --     capabilities = capabilities,
-        -- }
-
-        -- require 'lspconfig'.sqlls.setup {
-        --     capabilities = capabilities,
-        -- }
-
-        -- require 'lspconfig'.eslint.setup {
-        --     capabilities = capabilities,
-        -- }
-
-        -- require 'lspconfig'.tsserver.setup {
-        --     capabilities = capabilities,
-        -- }
-
-        -- require 'lspconfig'.cssls.setup {
-        --     capabilities = capabilities,
-        -- }
-
-        -- require 'lspconfig'.tailwindcss.setup {
-        --     capabilities = capabilities,
-        -- }
-
-        vim.diagnostic.config({
-            virtual_text = false,
-        })
-
-        vim.o.updatetime = 250
-
-        -- Old command for float, not being used and replaced with the below command
-        -- vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
-
-        -- BUG: disabling for now as it interferes with lspsaga
-        -- NOTE: Show diagnostics in a pop-up window on hover
-        -- SOURCE: https://www.reddit.com/r/neovim/comments/mqspfo/is_there_any_way_to_hide_the_diagnostic_from/
-        -- _G.LspDiagnosticsPopupHandler = function()
-        --     local current_cursor = vim.api.nvim_win_get_cursor(0)
-        --     local last_popup_cursor = vim.w.lsp_diagnostics_last_cursor or { nil, nil }
-
-        --     -- Show the popup diagnostics window,
-        --     -- but only once for the current cursor location (unless moved afterwards).
-        --     if not (current_cursor[1] == last_popup_cursor[1] and current_cursor[2] == last_popup_cursor[2]) then
-        --         vim.w.lsp_diagnostics_last_cursor = current_cursor
-        --         vim.diagnostic.open_float(0, { scope = "cursor" }) -- for neovim 0.6.0+, replaces show_{line,position}_diagnostics
-        --     end
-        -- end
-        -- vim.cmd([[
-        -- augroup LSPDiagnosticsOnHover
-        -- autocmd!
-        -- autocmd CursorHold *   lua _G.LspDiagnosticsPopupHandler()
-        -- augroup END
-        -- ]])
+        -- sql
+        -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#sqlls
+        -- https://github.com/joe-re/sql-language-server
+        lspconfig.sqlls.setup {
+            capabilities = capabilities,
+            on_attach = on_attach,
+            cmd = { "sql-language-server", "up", "--method", "stdio" },
+            filetypes = { "sql", "mysql" },
+            root_dir = function()
+                return vim.loop.cwd()
+            end,
+        }
 
         -- TEST:
         -- print("Hello from lazy lspconfig")
