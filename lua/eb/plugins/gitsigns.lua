dbfb14aa653b22abb0ed6df82356849f21cbea60
eb0687 <ebrahim0687@gmail.com>
2023-10-27 19:31:55 +0300

    chore(ui): update icons for gitsigns

--- lua/eb/plugins/gitsigns.lua
+++ lua/eb/plugins/gitsigns.lua
@@ -21,10 +21,10 @@ return {
             signs = {
                 add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
                 change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-                delete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+                delete = { hl = "GitSignsDelete", text = " ", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
                 topdelete = {
                     hl = "GitSignsDelete",
-                    text = "契",
+                    text = " ",
                     numhl = "GitSignsDeleteNr",
                     linehl = "GitSignsDeleteLn"
                 },
@@ -69,9 +69,6 @@ return {
             yadm = {
                 enable = false,
             },
-
-
-
         })
 
         -- KEYMAPS
