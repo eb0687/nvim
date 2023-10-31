0b9138cb9d5c1a54a36f034b64afeb4c168d0971
eb0687 <ebrahim0687@gmail.com>
2023-10-31 10:13:13 +0300

    chore: update icon for gitsigns delete

--- lua/eb/plugins/gitsigns.lua
+++ lua/eb/plugins/gitsigns.lua
@@ -21,7 +21,7 @@ return {
             signs = {
                 add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
                 change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-                delete = { hl = "GitSignsDelete", text = " ", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+                delete = { hl = "GitSignsDelete", text = "▎", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
                 topdelete = {
                     hl = "GitSignsDelete",
                     text = " ",
