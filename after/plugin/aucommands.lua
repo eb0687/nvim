e3f877140aafb493da0bd86fc08edbc84bf8acfd
eb0687 <ebrahim0687@gmail.com>
2023-11-03 15:38:36 +0300

    feat: create lazy-lock snapshots
    Use the command :BrowseSnapshots to view snapshots

--- after/plugin/aucommands.lua
+++ after/plugin/aucommands.lua
@@ -16,3 +16,26 @@ end
 user_command('HelloWorld', user_prompt, {
     desc = 'this a hello world user command test'
 })
+
+-- SOURCE: https://dev.to/vonheikemen/lazynvim-how-to-revert-a-plugin-back-to-a-previous-version-1pdp
+local lazy_cmds = vim.api.nvim_create_augroup('lazy_cmds', { clear = true })
+local snapshot_dir = vim.fn.stdpath('data') .. '/plugin-snapshot'
+local lockfile = vim.fn.stdpath('config') .. '/lazy-lock.json'
+
+vim.api.nvim_create_user_command(
+    'BrowseSnapshots',
+    'edit ' .. snapshot_dir,
+    {}
+)
+
+vim.api.nvim_create_autocmd('User', {
+    group = lazy_cmds,
+    pattern = 'LazyUpdatePre',
+    desc = 'Backup lazy.nvim lockfile',
+    callback = function(event)
+        vim.fn.mkdir(snapshot_dir, 'p')
+        local snapshot = snapshot_dir .. os.date('/%Y-%m-%dT%H:%M:%S.json')
+
+        vim.loop.fs_copyfile(lockfile, snapshot)
+    end,
+})
