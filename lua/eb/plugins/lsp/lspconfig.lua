0bbe9a0728355a3bce18673e951734c7a4cefd7d
eb0687 <ebrahim0687@gmail.com>
2024-07-03 00:56:34 +0300

    chore: add dependency for lspconfig
    added nvim-lsp-file-operations to automatically rename imports when
    changing folder names

--- lua/eb/plugins/lsp/lspconfig.lua
+++ lua/eb/plugins/lsp/lspconfig.lua
@@ -9,6 +9,10 @@
 return {
     "neovim/nvim-lspconfig",
     event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        { "antosha417/nvim-lsp-file-operations", config = true },
+    },
     config = function()
         local lspconfig = require("lspconfig")
         local util = require("lspconfig/util")
