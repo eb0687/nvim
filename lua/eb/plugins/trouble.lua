35cd325cfb06fa47c87f8586eafe4787a9a74216
eb0687 <ebrahim0687@gmail.com>
2024-07-03 23:37:14 +0300

    chore: update keymaps

--- lua/eb/plugins/trouble.lua
+++ lua/eb/plugins/trouble.lua
@@ -4,9 +4,9 @@
 return {
     "folke/trouble.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
-    config = function ()
+    config = function()
         local trouble = require('trouble')
-        trouble.setup{}
+        trouble.setup {}
 
         local keymap = function(keys, func, desc)
             if desc then
@@ -16,13 +16,10 @@ return {
             vim.keymap.set('n', keys, func, { desc = desc })
         end
 
-        keymap("<leader>to",":Trouble todo toggle focus=true filter.buf=0<CR>","Toggle todo list")
-        keymap("<leader>tO",":Trouble todo toggle focus=true<CR>","Toggle todo list")
-        keymap("<leader>tt",":Trouble diagnostics toggle focus=true<CR>","Open/Close trouble list")
-        keymap("<leader>tB",":Trouble diagnostics toggle focus=true<CR>","Diagnostics in current buffer")
-        keymap("<leader>tb",":Trouble diagnostics toggle focus=true filter.buf=0<CR>","Diagnostics in current buffer")
-        keymap("<leader>te",":Trouble diagnostics toggle focus=true filter.severity = vim.diagnostic.severity.ERROR<CR>","Show ERROR in current buffer")
-        keymap("<leader>tq",":Trouble quickfix toggle focus=true<CR>","Toggle quickfix list")
-        keymap("<leader>ts",":Trouble symbols toggle focus=true<CR>","Toggle symbols list")
+        keymap("<leader>to", ":Trouble todo toggle focus=true<CR>", "Toggle todo list")
+        keymap("<leader>tt", ":Trouble diagnostics toggle focus=true<CR>", "Toggle trouble list")
+        keymap("<leader>tq", ":Trouble quickfix toggle focus=true<CR>", "Toggle quickfix list")
+        keymap("<leader>ts", ":Trouble symbols toggle focus=true<CR>", "Toggle symbols list")
+        keymap("<leader>tl", ":Trouble loclist toggle focus=true<CR>", "Toggle symbols list")
     end
 }
