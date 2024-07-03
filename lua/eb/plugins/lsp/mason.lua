bd4bc66578b00e2bcfba20e80efb21b3d9abff67
eb0687 <ebrahim0687@gmail.com>
2024-07-04 01:50:37 +0300

    chore: formatting

--- lua/eb/plugins/lsp/mason.lua
+++ lua/eb/plugins/lsp/mason.lua
@@ -9,11 +9,13 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     dependencies = {
         "williamboman/mason-lspconfig.nvim", -- https://github.com/williamboman/mason-lspconfig.nvim
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
     },
 
     config = function()
         local mason = require("mason")
         local mason_lspconfig = require("mason-lspconfig")
+        local mason_tool_installer = require("mason-tool-installer")
 
         -- NOTE: setup calls required for both mason & mason-lspconfig
         -- Source: https://github.com/williamboman/mason-lspconfig.nvim#setup
@@ -44,6 +46,15 @@ return {
                 "gopls",
             },
             automatic_installation = true,
+        })
+
+        mason_tool_installer.setup({
+            ensure_installed = {
+                "prettier", -- prettier formatter
+                "stylua", -- lua formatter
+                "black", -- python formatter
+                "pylint",
+            },
         })
     end,
 }
