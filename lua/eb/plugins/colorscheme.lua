37405db86f78a0fed1c7bac6db5764347d32ec6b
eb0687 <ebrahim0687@gmail.com>
2024-07-05 14:03:40 +0300

    chore: formatting

--- lua/eb/plugins/colorscheme.lua
+++ lua/eb/plugins/colorscheme.lua
@@ -7,7 +7,7 @@
 -- https://github.com/sainnhe/gruvbox-material
 
 return {
-    'sainnhe/gruvbox-material', -- gruvbox material colorscheme
+    "sainnhe/gruvbox-material", -- gruvbox material colorscheme
     priority = 1000,
     lazy = false,
     config = function()
@@ -37,48 +37,50 @@ return {
         -- of colors
 
         -- Custom highlights
-        nvim_set_hl(0, 'NormalFloat', { bg = '#32302F' })
-        nvim_set_hl(0, 'FloatBorder', { bg = '#32302F', fg = '#000000' })
+        nvim_set_hl(0, "NormalFloat", { bg = "#32302F" })
+        nvim_set_hl(0, "FloatBorder", { bg = "#32302F", fg = "#000000" })
 
         -- Flash.nvim
-        nvim_set_hl(0, 'FlashBackdrop', { bg = '#191919', fg = '#665C54' })
-        nvim_set_hl(0, 'FlashLabel', { bg = '#EA6962', fg = '#191919' })
-        nvim_set_hl(0, 'FlashPrompt', { bg = '#EA6962', fg = '#191919' })
-        nvim_set_hl(0, 'FlashPromptIcon', { bg = '#EA6962', fg = '#191919' })
-        nvim_set_hl(0, 'FlashCurrent', { bg = '#EA6962' })
+        nvim_set_hl(0, "FlashBackdrop", { bg = "#191919", fg = "#665C54" })
+        nvim_set_hl(0, "FlashLabel", { bg = "#EA6962", fg = "#191919" })
+        nvim_set_hl(0, "FlashPrompt", { bg = "#EA6962", fg = "#191919" })
+        nvim_set_hl(0, "FlashPromptIcon", { bg = "#EA6962", fg = "#191919" })
+        nvim_set_hl(0, "FlashCurrent", { bg = "#EA6962" })
 
         -- Hydra
-        nvim_set_hl(0, 'HydraPink', { bg = '#EA6962', fg = '#191919', bold = true })
+        nvim_set_hl(0, "HydraPink", { bg = "#EA6962", fg = "#191919", bold = true })
 
         -- CMP
         -- gray
-        nvim_set_hl(0, 'CmpItemAbbrDeprecated', { bg = 'NONE', strikethrough = true, fg = '#808080' })
+        nvim_set_hl(0, "CmpItemAbbrDeprecated", { bg = "NONE", strikethrough = true, fg = "#808080" })
         -- blue
-        nvim_set_hl(0, 'CmpItemAbbrMatch', { bg = 'NONE', fg = '#E78A4E' })
-        nvim_set_hl(0, 'CmpItemAbbrMatchFuzzy', { link = 'CmpIntemAbbrMatch' })
+        nvim_set_hl(0, "CmpItemAbbrMatch", { bg = "NONE", fg = "#E78A4E" })
+        nvim_set_hl(0, "CmpItemAbbrMatchFuzzy", { link = "CmpIntemAbbrMatch" })
         -- light blue
-        nvim_set_hl(0, 'CmpItemKindVariable', { bg = 'NONE', fg = '#EA6962' })
-        nvim_set_hl(0, 'CmpItemKindInterface', { link = 'CmpItemKindVariable' })
-        nvim_set_hl(0, 'CmpItemKindText', { link = 'CmpItemKindVariable' })
+        nvim_set_hl(0, "CmpItemKindVariable", { bg = "NONE", fg = "#EA6962" })
+        nvim_set_hl(0, "CmpItemKindInterface", { link = "CmpItemKindVariable" })
+        nvim_set_hl(0, "CmpItemKindText", { link = "CmpItemKindVariable" })
         -- pink
-        nvim_set_hl(0, 'CmpItemKindFunction', { bg = 'NONE', fg = '#89B482' })
-        nvim_set_hl(0, 'CmpItemKindMethod', { link = 'CmpItemKindFunction' })
+        nvim_set_hl(0, "CmpItemKindFunction", { bg = "NONE", fg = "#89B482" })
+        nvim_set_hl(0, "CmpItemKindMethod", { link = "CmpItemKindFunction" })
         -- front
-        nvim_set_hl(0, 'CmpItemKindKeyword', { bg = 'NONE', fg = '#7DAEA3' })
-        nvim_set_hl(0, 'CmpItemKindProperty', { link = 'CmpItemKindKeyword' })
-        nvim_set_hl(0, 'CmpItemKindUnit', { link = 'CmpItemKindKeyword' })
+        nvim_set_hl(0, "CmpItemKindKeyword", { bg = "NONE", fg = "#7DAEA3" })
+        nvim_set_hl(0, "CmpItemKindProperty", { link = "CmpItemKindKeyword" })
+        nvim_set_hl(0, "CmpItemKindUnit", { link = "CmpItemKindKeyword" })
         -- lspsaga
-        nvim_set_hl(0, 'ActionPreviewTitle', { bg = '#32302f', fg = '#89B482' })
-        nvim_set_hl(0, 'SagaText', { bg = '#32302F', fg = '#EA6962' })
+        nvim_set_hl(0, "ActionPreviewTitle", { bg = "#32302f", fg = "#89B482" })
+        nvim_set_hl(0, "SagaText", { bg = "#32302f", fg = "#EA6962" })
+        nvim_set_hl(0, "HoverBorder", { bg = "#32302f" })
+        nvim_set_hl(0, "HoverNormal", { bg = "#32302f", fg = "#EA6962" })
         -- muticursors
-        nvim_set_hl(0, 'Multicursor', { bg = '#32302F', fg = '#EA6962', default = true })
-        nvim_set_hl(0, 'MulticursorMain', { bg = '#32302F', fg = '#EA6962', bold = true, default = true })
+        nvim_set_hl(0, "Multicursor", { bg = "#32302F", fg = "#EA6962", default = true })
+        nvim_set_hl(0, "MulticursorMain", { bg = "#32302F", fg = "#EA6962", bold = true, default = true })
         -- barbar
-        nvim_set_hl(0, 'BufferCurrent', { bg = '#252423', fg = '#E78A4E' })
-        nvim_set_hl(0, 'BufferVisible', { bg = '#252423', fg = '#84776A' })
-        nvim_set_hl(0, 'BufferInactive', { bg = '#252423', fg = '#84776A' })
+        nvim_set_hl(0, "BufferCurrent", { bg = "#252423", fg = "#E78A4E" })
+        nvim_set_hl(0, "BufferVisible", { bg = "#252423", fg = "#84776A" })
+        nvim_set_hl(0, "BufferInactive", { bg = "#252423", fg = "#84776A" })
 
         -- TEST:
         -- print('Hello from colorscheme lazy')
-    end
+    end,
 }
