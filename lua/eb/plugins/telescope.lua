f0961fb60cdc6aa0d475ff5924ddb586c40236dd
eb0687 <ebrahim0687@gmail.com>
2024-07-07 11:33:50 +0300

    chore: formatting

--- lua/eb/plugins/telescope.lua
+++ lua/eb/plugins/telescope.lua
@@ -7,28 +7,29 @@
 -- https://github.com/nvim-telescope/telescope.nvim
 
 return {
-    'nvim-telescope/telescope.nvim',
-    event = { 'BufReadPre', 'BufNewFile' },
-    cmd = 'Telescope',
-    branch = '0.1.x',
+    "nvim-telescope/telescope.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    cmd = "Telescope",
+    branch = "0.1.x",
     dependencies = {
         {
-            'nvim-tree/nvim-web-devicons',
+            "nvim-tree/nvim-web-devicons",
             lazy = true,
         },
-        'nvim-lua/plenary.nvim',
-        { 'nvim-tree/nvim-web-devicons', lazy = true },
-        'nvim-telescope/telescope-github.nvim',
-        'ThePrimeagen/harpoon',
+        "nvim-lua/plenary.nvim",
+        { "nvim-tree/nvim-web-devicons", lazy = true },
+        "nvim-telescope/telescope-github.nvim",
+        "ThePrimeagen/harpoon",
         {
-            'piersolenski/telescope-import.nvim',
-            ft = { 'python', 'javascript', 'lua' },
+            "piersolenski/telescope-import.nvim",
+            ft = { "python", "javascript", "lua" },
         },
     },
 
     config = function()
         local telescope = require("telescope")
         local open_with_trouble = require("trouble.sources.telescope").open
+        -- TODO: look up how to utilize this command
         -- Use this to add more results without clearing the trouble list
         local add_to_trouble = require("trouble.sources.telescope").add
 
@@ -80,20 +81,19 @@ return {
                         ["<C-t>"] = open_with_trouble,
                     },
                     n = {
-                        ["<C-t>"] = open_with_trouble
+                        ["<C-t>"] = open_with_trouble,
                     },
-                }
-
+                },
             },
             pickers = {
                 find_files = {
-                    hidden = true
-                }
+                    hidden = true,
+                },
             },
             extensions = {
                 tmuxinator = {
-                    select_action = 'switch',
-                    stop_action = 'stop',
+                    select_action = "switch",
+                    stop_action = "stop",
                     disable_icons = false,
                 },
                 fzy_native = {
@@ -104,56 +104,58 @@ return {
                     -- Add imports to the top of the file keeping the cursor in place
                     insert_at_top = true,
                 },
-
             },
 
-            telescope.load_extension('notify'),
-            telescope.load_extension('harpoon'),
-            telescope.load_extension('import'),
-            telescope.load_extension('refactoring'),
+            telescope.load_extension("notify"),
+            telescope.load_extension("harpoon"),
+            telescope.load_extension("import"),
+            telescope.load_extension("refactoring"),
         })
 
         -- KEYMAPS
         local keymap = function(keys, func, desc)
             if desc then
-                desc = 'TELESCOPE: ' .. desc
+                desc = "TELESCOPE: " .. desc
             end
 
-            vim.keymap.set('n', keys, func, { desc = desc })
+            vim.keymap.set("n", keys, func, { desc = desc })
         end
 
-        local custom_theme = require('telescope.themes').get_dropdown({
+        local custom_theme = require("telescope.themes").get_dropdown({
             previewer = false,
         })
 
-        local telescope_builtin = require('telescope.builtin')
+        local telescope_builtin = require("telescope.builtin")
 
         -- keybinds
-        keymap("<leader>ff", telescope_builtin.find_files, 'Find Files in local folder only recursively')
-        keymap("<leader>fo", telescope_builtin.oldfiles, 'Find recently Opened files')
-        keymap("<leader>fk", telescope_builtin.keymaps, 'Find Keymaps')
-        keymap("<leader>fh", telescope_builtin.help_tags, 'Find in Help tags')
-        keymap("<leader>fp", telescope_builtin.man_pages, 'Find in man Pages')
-        keymap("<leader>fm", telescope_builtin.marks, 'Find in vim marks')
-        keymap("<leader>fg", telescope_builtin.git_files, 'Find files in Git repository')
-        keymap("gr", telescope_builtin.lsp_references, 'Find references [LSP]')
-        keymap("gd", telescope_builtin.lsp_definitions, 'Find definitions [LSP]')
-        keymap("gD", telescope_builtin.lsp_type_definitions, 'Find type definitions [LSP]')
-        keymap("<leader>ds", telescope_builtin.lsp_document_symbols, 'Find document symbols [LSP]')
-        keymap("<leader>co", telescope_builtin.quickfix, 'View all items in the quickfix list')
+        keymap("<leader>ff", telescope_builtin.find_files, "Find Files in local folder only recursively")
+        keymap("<leader>fo", telescope_builtin.oldfiles, "Find recently Opened files")
+        keymap("<leader>fk", telescope_builtin.keymaps, "Find Keymaps")
+        keymap("<leader>fh", telescope_builtin.help_tags, "Find in Help tags")
+        keymap("<leader>fp", telescope_builtin.man_pages, "Find in man Pages")
+        keymap("<leader>fm", telescope_builtin.marks, "Find in vim marks")
+        keymap("<leader>fg", telescope_builtin.git_files, "Find files in Git repository")
+        keymap("gr", telescope_builtin.lsp_references, "Find references [LSP]")
+        keymap("gd", telescope_builtin.lsp_definitions, "Find definitions [LSP]")
+        keymap("gD", telescope_builtin.lsp_type_definitions, "Find type definitions [LSP]")
+        keymap("<leader>ds", telescope_builtin.lsp_document_symbols, "Find document symbols [LSP]")
+        keymap("<leader>co", telescope_builtin.quickfix, "View all items in the quickfix list")
         -- BUG: this does not reload modules but does show the notification
-        keymap("<leader>fr", "<cmd>:lua require('eb.utils.telescope_reload').reload()<CR>",
-            'Reload nvim plugin using telescope')
+        keymap(
+            "<leader>fr",
+            "<cmd>:lua require('eb.utils.telescope_reload').reload()<CR>",
+            "Reload nvim plugin using telescope"
+        )
 
         keymap("<leader>fs", function()
             telescope_builtin.grep_string({
-                search = vim.fn.input('Grep for > ')
+                search = vim.fn.input("Grep for > "),
             })
-        end, 'Find using Grep')
+        end, "Find using Grep")
 
         keymap("<leader>fn", function()
             telescope_builtin.find_files({
-                prompt_title = '[ VIMRC ]',
+                prompt_title = "[ VIMRC ]",
                 file_ignore_patterns = {
                     "^.git/",
                     "node_modules\\",
@@ -166,13 +168,13 @@ return {
                     "%.exe",
                     "%.pyc",
                 },
-                cwd = '~/.config/nvim/'
+                cwd = "~/.config/nvim/",
             })
-        end, 'Find in Nvim configs')
+        end, "Find in Nvim configs")
 
         keymap("<leader>fF", function()
             telescope_builtin.find_files({
-                prompt_title = '[ Search HOME ]',
+                prompt_title = "[ Search HOME ]",
                 file_ignore_patterns = {
                     "^.git/",
                     "node_modules\\",
@@ -185,40 +187,38 @@ return {
                     "%.exe",
                     "%.pyc",
                 },
-                cwd = '~/'
+                cwd = "~/",
             })
-        end, 'Find Files in home directory')
+        end, "Find Files in home directory")
 
         keymap("<leader>fw", function()
             telescope_builtin.grep_string({
-                search = vim.fn.expand('<cword>'),
-                grep_open_files = true
+                search = vim.fn.expand("<cword>"),
+                grep_open_files = true,
             })
-        end, 'Find Word under cursor')
+        end, "Find Word under cursor")
 
         keymap("<leader>fb", function()
             telescope_builtin.buffers(custom_theme)
-        end, 'Find in existing Buffers')
+        end, "Find in existing Buffers")
 
         keymap("<leader>fsh", function()
             telescope_builtin.search_history(custom_theme)
-        end, 'Find Search History')
+        end, "Find Search History")
 
-        keymap('<leader>fcb', function()
+        keymap("<leader>fcb", function()
             telescope_builtin.current_buffer_fuzzy_find(custom_theme)
-        end, 'Find Current Buffer')
+        end, "Find Current Buffer")
 
         keymap("<leader>fch", function()
             telescope_builtin.command_history(custom_theme)
-        end, 'Find Command History')
+        end, "Find Command History")
 
-        vim.keymap.set(
-            { "n", "x" },
-            "<leader>re",
-            function() require('telescope').extensions.refactoring.refactors() end
-        )
+        vim.keymap.set({ "n", "x" }, "<leader>re", function()
+            require("telescope").extensions.refactoring.refactors()
+        end)
 
         -- TEST:
         -- print("Hello from lazy telescope")
-    end
+    end,
 }
