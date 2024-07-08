d1ba836536a29dfacc7adab34c04a70f6ee8e44c
eb0687 <ebrahim0687@gmail.com>
2024-07-08 13:39:46 +0300

    feat: add new custom lua commands

--- lua/eb/init.lua
+++ lua/eb/init.lua
@@ -1,7 +1,10 @@
 require("eb.options")
 require("eb.keymaps")
 require("eb.lazy")
+
+-- custom commands
 require("eb.utils.reload")
+require("eb.utils.rotate_window")
 
 -- TEST:
 -- print("hello from EB")
