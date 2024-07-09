d6cfd07b37e81b187d82e24d36aefea5908b4937
eb0687 <ebrahim0687@gmail.com>
2024-07-09 11:52:59 +0300

    chore: cleanup

--- lua/eb/plugins/hop.lua
+++ lua/eb/plugins/hop.lua
@@ -7,84 +7,88 @@
 -- https://github.com/smoka7/hop.nvim
 
 return {
-    'smoka7/hop.nvim',
+    "smoka7/hop.nvim",
     version = "*",
     enabled = false,
 
     config = function()
-        local hop = require('hop')
+        local hop = require("hop")
+        local custom_helpers = require("eb.utils.custom_helpers")
+        local keymap_normal = custom_helpers.keymap_normal
+        local keymap_visual = custom_helpers.keymap_visual
+        local keymap = custom_helpers.keymap
 
         -- SETUP
         -- source: https://github.com/smoka7/hop.nvim/wiki/Configuration
         hop.setup({
-            keys = 'etovxqpdygfblzhckisuran',
+            keys = "etovxqpdygfblzhckisuran",
             jump_on_sole_occurrence = false,
         })
 
         -- KEYMAPS
-        local keymap = function(keys, func, desc)
-            if desc then
-                desc = 'HOP: ' .. desc
-            end
+        -- local keymap = function(keys, func, desc)
+        --     if desc then
+        --         desc = "HOP: " .. desc
+        --     end
 
-            vim.keymap.set('', keys, func, { desc = desc })
-        end
+        --     vim.keymap.set("", keys, func, { desc = desc })
+        -- end
 
-        local keymap_n = function(keys, func, desc)
-            if desc then
-                desc = 'HOP: ' .. desc
-            end
+        -- local keymap_n = function(keys, func, desc)
+        --     if desc then
+        --         desc = "HOP: " .. desc
+        --     end
 
-            vim.keymap.set('n', keys, func, { desc = desc })
-        end
+        --     vim.keymap.set("n", keys, func, { desc = desc })
+        -- end
 
-        local keymap_v = function(keys, func, desc)
-            if desc then
-                desc = 'HOP: ' .. desc
-            end
+        -- local keymap_v = function(keys, func, desc)
+        --     if desc then
+        --         desc = "HOP: " .. desc
+        --     end
 
-            vim.keymap.set('v', keys, func, { desc = desc })
-        end
+        --     vim.keymap.set("v", keys, func, { desc = desc })
+        -- end
 
         -- source: https://github.com/smoka7/hop.nvim/wiki/Commands
-        keymap_n("<leader>hw", ":HopWord<CR>", '[H]op [W]ord')
-        keymap_n("<leader>hl", ":HopLine<CR>", '[H]op [L]ine')
-        keymap_n("<leader>hp", ":HopPattern<CR>", '[H]op [P]attern')
-        keymap_v("<leader>hw", ":HopWord<CR>", '[H]op [W]ord')
-        keymap_v("<leader>hl", ":HopLine<CR>", '[H]op [L]ine')
+        keymap_normal("<leader>hw", ":HopWord<CR", "HOP", true, "[H]op [W]ord")
+        keymap_normal("<leader>hl", ":HopLine<CR", "HOP", true, "[H]op [L]ine")
+        keymap_normal("<leader>hp", ":HopPattern<CR", "HOP", true, "[H]op [P]attern")
+        keymap_visual("<leader>hw", ":HopWord<CR", "HOP", true, "[H]op [W]ord")
+        keymap_visual("<leader>hl", ":HopLine<CR", "HOP", true, "[H]op [L]ine")
 
         -- VIM MOTIONS
-        keymap('f', function()
-            require('hop').hint_char1({
-                direction = require('hop.hint').HintDirection.AFTER_CURSOR,
-                current_line_only = true
-            })
-        end, 'Hop forward to character')
-
-        keymap('F', function()
-            require('hop').hint_char1({
-                direction = require('hop.hint').HintDirection.BEFORE_CURSOR,
+        keymap("f", function()
+            require("hop").hint_char1({
+                direction = require("hop.hint").HintDirection.AFTER_CURSOR,
                 current_line_only = true,
             })
-        end, 'Hop backward to character')
+        end, "HOP", true, "Hop forward to character")
 
-        keymap('t', function()
-            require('hop').hint_char1({
-                direction = require('hop.hint').HintDirection.AFTER_CURSOR,
+        keymap("F", function()
+            require("hop").hint_char1({
+                direction = require("hop.hint").HintDirection.BEFORE_CURSOR,
                 current_line_only = true,
-                hint_offset = -1
             })
-        end, 'Hop forward to character but place cursor behind character')
+        end, "HOP", true, "Hop backward to character")
 
-        keymap('T', function()
-            require('hop').hint_char1({
-                direction = require('hop.hint').HintDirection.BEFORE_CURSOR,
+        keymap("t", function()
+            require("hop").hint_char1({
+                direction = require("hop.hint").HintDirection.AFTER_CURSOR,
                 current_line_only = true,
-                hint_offset = 1
+                hint_offset = -1,
             })
-        end, 'Hop backward to character but place cursor infront of character')
+        end, "HOP", true, "Hop forward to character but place cursor behind character")
+
+        keymap("T", function()
+            require("hop").hint_char1({
+                direction = require("hop.hint").HintDirection.BEFORE_CURSOR,
+                current_line_only = true,
+                hint_offset = 1,
+            })
+        end, "HOP", true, "Hop backward to character but place cursor infront of character")
 
         -- TEST:
         -- print('Hello from lazy hop')
-    end
+    end,
 }
