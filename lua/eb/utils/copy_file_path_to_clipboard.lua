e4ac115d390f52a0b5484bdcdfe603f4c479bafc
eb0687 <ebrahim0687@gmail.com>
2024-07-09 14:53:10 +0300

    chore: fix typo

--- lua/eb/utils/copy_file_path_to_clipboard.lua
+++ lua/eb/utils/copy_file_path_to_clipboard.lua
@@ -1,4 +1,4 @@
--- https://github.com/dmmulroy/kickstart.nix/blob/dcb116a6bceb73285636cfe57a93b3a636b4bf6f/config/nvim/lua/user/copy_file_path_to_clipboard.lua
+-- https://github.com/dmmulroy/kickstart.nix/blob/main/config/nvim/lua/user/copy_file_path_to_clipboard.lua
 vim.api.nvim_create_user_command("CopyFilePathToClipboard", function()
     vim.fn.setreg("+", vim.fn.expand("%:p"))
 end, {})
