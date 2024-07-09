948c2c20bb3431294a4f6a3b1443e79015fa4826
eb0687 <ebrahim0687@gmail.com>
2024-07-09 10:55:45 +0300

    chore: cleanup

--- lua/eb/plugins/auto-sessions.lua
+++ lua/eb/plugins/auto-sessions.lua
@@ -27,8 +27,18 @@ return {
             vim.keymap.set("n", keys, func, { desc = desc })
         end
 
-        keymap("<leader>wr", "<cmd>SessionRestore<CR>", "Restore session for cwd") -- restore last workspace session for current directory
-        keymap("<leader>ws", "<cmd>SessionSave<CR>", "Save session for auto session root dir") -- save workspace session for current working directory
-        keymap("<leader>wl", require("auto-session.session-lens").search_session, "List all sessions in Telescope")
+        keymap_normal("<leader>wr", "<cmd>SessionRestore<CR>", "AUTO SESSION", true, "Restore session for cwd")
+        keymap_normal(
+            "<leader>ws",
+            "<cmd>SessionSave<CR>",
+            "AUTO SESSION",
+            true,
+            "Save session for auto session root dir"
+        ) -- save workspace session for current working directory
+        keymap_normal(
+            "<leader>wl",
+            require("auto-session.session-lens").search_session,
+            "List all sessions in Telescope"
+        )
     end,
 }
