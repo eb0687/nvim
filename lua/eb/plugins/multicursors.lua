59d272e8d6cd4312fe10a10db8dc7b0d3a7b5c5f
eb0687 <ebrahim0687@gmail.com>
2024-07-09 12:02:07 +0300

    chore: formatting

--- lua/eb/plugins/multicursors.lua
+++ lua/eb/plugins/multicursors.lua
@@ -5,44 +5,46 @@
 -- |_| |_| |_|\__,_|_|\__|_|\___|\__,_|_|  |___/\___/|_|  |___/
 -- https://github.com/smoka7/multicursors.nvim
 
+-- NOTE: do i really need this?
+
 -- lazy.nvim:
 return {
     "smoka7/multicursors.nvim",
     dependencies = {
-        'smoka7/hydra.nvim',
+        "smoka7/hydra.nvim",
     },
     opts = {},
-    cmd = { 'MCstart', 'MCvisual', 'MCclear', 'MCpattern', 'MCvisualPattern', 'MCunderCursor' },
+    cmd = { "MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor" },
     keys = {
         {
-            mode = { 'v', 'n' },
-            '<Leader>m',
-            '<cmd>MCstart<cr>',
-            desc = 'Create a selection for selected text or word under the cursor',
+            mode = { "v", "n" },
+            "<Leader>m",
+            "<cmd>MCstart<cr>",
+            desc = "Create a selection for selected text or word under the cursor",
         },
     },
 
     config = function()
-        local multicursors = require('multicursors')
+        local multicursors = require("multicursors")
 
-        multicursors.setup {
+        multicursors.setup({
             DEBUG_MODE = false,
             create_commands = true, -- create Multicursor user commands
-            updatetime = 50,        -- selections get updated if this many milliseconds nothing is typed in the insert mode see :help updatetime
-            nowait = true,          -- see :help :map-nowait
+            updatetime = 50, -- selections get updated if this many milliseconds nothing is typed in the insert mode see :help updatetime
+            nowait = true, -- see :help :map-nowait
             mode_keys = {
-                append = 'a',
-                change = 'c',
-                extend = 'e',
-                insert = 'i',
+                append = "a",
+                change = "c",
+                extend = "e",
+                insert = "i",
             }, -- set bindings to start these modes
             -- normal_keys = normal_keys,
             -- insert_keys = insert_keys,
             -- extend_keys = extend_keys,
             -- see :help hydra-config.hint
             hint_config = {
-                border = 'none',
-                position = 'bottom',
+                border = "none",
+                position = "bottom",
             },
 
             -- accepted values:
@@ -54,7 +56,6 @@ return {
                 insert = false,
                 extend = false,
             },
-
-        }
-    end
+        })
+    end,
 }
