693660e5f93dd7412084502498da264e29215ea1
eb0687 <ebrahim0687@gmail.com>
2024-07-10 15:35:22 +0300

    feat: add neogit.nvim
    testing this plugin out for improving my git workflow

---
+++ lua/eb/plugins/neogit.lua
@@ -0,0 +1,15 @@
+-- neogit.nvim
+-- https://github.com/NeogitOrg/neogit
+
+return {
+    "NeogitOrg/neogit",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "sindrets/diffview.nvim",
+        "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+        local neogit = require("neogit")
+        neogit.setup({})
+    end,
+}
