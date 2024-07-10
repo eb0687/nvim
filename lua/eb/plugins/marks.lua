93e88b2bed6f88d07fb8209df67b6a38bc857920
eb0687 <ebrahim0687@gmail.com>
2024-07-10 12:35:30 +0300

    feat: add marks.nvim for easier marks management

---
+++ lua/eb/plugins/marks.lua
@@ -0,0 +1,10 @@
+-- marks.nvim
+-- https://github.com/chentoast/marks.nvim
+
+return {
+    "chentoast/marks.nvim",
+    config = function()
+        local marks = require("marks")
+        marks.setup({})
+    end,
+}
