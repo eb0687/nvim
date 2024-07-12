7b4e48b138fa7aeccc1c877adb97cd5fa30198f1
eb0687 <ebrahim0687@gmail.com>
2024-07-12 12:58:29 +0300

    fix: conditional install

--- lua/eb/lazy.lua
+++ lua/eb/lazy.lua
@@ -37,7 +37,13 @@ local opts = {
 local utils = require("eb.utils.custom_helpers")
 local hostname = utils.get_hostname()
 
-if hostname == "JIGA" or "eb-t490" then
+if hostname == "JIGA" then
+    require("lazy").setup({
+        { import = "eb.plugins" }, -- plugins configs managed by lazy
+        { import = "eb.plugins.lsp" }, -- lsp configs managed by lazy
+        { import = "eb.plugins.jiga" }, -- lsp configs managed by lazy
+    }, opts)
+elseif hostname == "eb-t490" then
     require("lazy").setup({
         { import = "eb.plugins" }, -- plugins configs managed by lazy
         { import = "eb.plugins.lsp" }, -- lsp configs managed by lazy
