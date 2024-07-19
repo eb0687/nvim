73112ddedc147f11baaa8ab9558dbf80c95016b1
eb0687 <ebrahim0687@gmail.com>
2024-07-19 19:51:37 +0300

    chore: add keymap to fix spelling

--- lua/eb/keymaps.lua
+++ lua/eb/keymaps.lua
@@ -98,6 +98,8 @@ keymap_silent(
 keymap_silent("n", "<leader>rw", ":RotateWindows<CR>", "Rotate windows")
 -- lazy
 keymap_silent("n", "<leader>zz", "<cmd>Lazy<CR>", "Lazy")
+-- fix word under cursor
+keymap_silent("n", "z0", "1z=", "Fix world under cursor")
 
 --
 -- NOTE: disabled for now, will use neogit instead
