c7f81721f6622b2f966377dfa88faf28b174f5aa
eb0687 <ebrahim0687@gmail.com>
2024-07-19 19:37:13 +0300

    feat: add close on q for specific buffer types

---
+++ after/plugin/close_on_q.lua
@@ -0,0 +1,23 @@
+-- https://github.com/jellydn/my-nvim-ide/blob/main/lua/config/autocmds.lua
+local function augroup(name)
+    return vim.api.nvim_create_augroup("nvim_ide_" .. name, { clear = true })
+end
+
+-- close some filetypes with <q>
+vim.api.nvim_create_autocmd("FileType", {
+    group = augroup("close_with_q"),
+    pattern = {
+        "help",
+        "lspinfo",
+        "notify",
+        "qf",
+        "startuptime",
+        "tsplayground",
+        "checkhealth",
+        "gitsigns.blame",
+    },
+    callback = function(event)
+        vim.bo[event.buf].buflisted = false
+        vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+    end,
+})
