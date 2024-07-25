5d2661dbec30fd16e7a579d9a02b24cdc6b6afdb
eb0687 <ebrahim0687@gmail.com>
2024-07-25 11:05:15 +0300

    chore: cleanup

--- lua/eb/keymaps.lua
+++ lua/eb/keymaps.lua
@@ -26,29 +26,37 @@ vim.g.mapleader = " "
 
 -- Source Nvim
 keymap_loud("n", "<leader>xo", ":luafile %<CR>", "Source NVIM")
+
 -- Save file
 -- keymap("n", "<leader>s", ":w<CR>", 'Save file')
 keymap_loud("n", "<leader>s", ":update<CR>", "Save file")
+
 -- Quit Nvim
 keymap_silent("n", "<leader>q", ":q!<CR>", "Quit Vim")
+
 -- Yank from current position to end of line
 keymap_silent("n", "Y", "yg$", "Yank to end of line")
+
 -- Create splits
 keymap_silent("n", "<leader>h", ":split<CR>", "Horizontal split")
 keymap_silent("n", "<leader>v", ":vsplit<CR>", "Vertical split")
+
 -- Resize buffer splits
 keymap_silent("n", "<C-A-Left>", ":vertical resize +3<CR>", "Vertical resize (+)")
 keymap_silent("n", "<C-A-Right>", ":vertical resize -3<CR>", "Vertical resize (-)")
 keymap_silent("n", "<C-A-Up>", ":resize +3<CR>", "Vertical resize (+)")
 keymap_silent("n", "<C-A-Down>", ":resize -3<CR>", "Vertical resize (-)")
+
 -- Buffer navigation
 keymap_silent("n", "<TAB>", ":bnext<CR>", "Next buffer")
 keymap_silent("n", "<S-TAB>", ":bprevious<CR>", "Previous buffer")
 keymap_silent("n", "<leader>bd", ":bdelete<CR>", "Delete focused buffer")
 keymap_silent("n", "<leader>bD", ":%bd|e#<CR>", "Delete all except for focused buffer")
 keymap_silent("n", "<leader>BD", ":%bd<CR>", "Delete all buffers")
+
 -- Highlight off
 keymap_silent("n", "<leader>ho", ":noh<CR>", "Highlight off")
+
 -- Search & Replace
 keymap_loud("n", "<C-s>", ":%s/", "Search and replace")
 keymap_silent("n", "n", "nzzzv", "Go to next search")
@@ -59,8 +67,10 @@ keymap_loud(
     [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
     "Search and replace word under cursor"
 )
+
 -- Automatically create the file if it does not exist
 keymap_silent("n", "gf", ":edit <cfile><CR>", "Go to file (creates file if missing)")
+
 -- Motions/Movement
 keymap_silent("n", "^", "0", "Go to beggining fo the line")
 keymap_silent("n", "0", "^", "Go to first non blank character on a line")
@@ -76,16 +86,20 @@ keymap_silent("n", "<C-i>", "<C-i>zz", "Navigate jumplist")
 keymap_silent("n", "<C-o>", "<C-o>zz", "Navigate jumplist")
 keymap_silent("n", "%", "%zz", "jump between matching parenthesis")
 keymap_silent("n", "#", "#zz", "jump to previous matching word")
+
 -- Remap for dealing with word wrap
 -- NOTE: SOURCE: https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua
 vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
 -- Quickfix list
 keymap_silent("n", "<leader>j", "<cmd>cnext<CR>zz", "Quickfix next")
 keymap_silent("n", "<leader>k", "<cmd>cprev<CR>zz", "Quickfix prev")
+
 -- Various
 keymap_silent("n", "<leader>xx", "<cmd>silent !chmod +x %<CR>", "Make executable")
 keymap_silent("n", "<leader>w", ":set wrap!<CR>", "Toggle wrap")
+
 -- keymap_silent("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>", 'Tmux sessionizer')
 keymap_silent("n", "<C-f>", "<cmd>silent !t<CR>", "Tmux sessionizer")
 keymap_silent("n", "<leader>tmss", "<cmd>silent !tmux neww tmss<CR>", "Switch Tmux session")
@@ -96,22 +110,12 @@ keymap_silent(
     "Open in web browser"
 )
 keymap_silent("n", "<leader>rw", ":RotateWindows<CR>", "Rotate windows")
--- lazy
+
+-- Lazy
 keymap_silent("n", "<leader>zz", "<cmd>Lazy<CR>", "Lazy")
--- spellchecker
+
+-- Spellchecker
 keymap_silent("n", "z0", "1z=", "Fix world under cursor")
-
---
--- NOTE: disabled for now, will use neogit instead
--- Vim fugitive
--- keymap_silent("n", "<leader>go", ":Git commit<CR>", "Git commit current file in a vim split")
-
--- TODO: needs more work
--- NOTE: temporarily disabling the below keymap and using CODE_RUNNER for script execution
--- keymap_silent("n","@P",function ()
---     vim.cmd('term bash %')
--- end,"Execute bash script")
--- keymap_silent("n","@P",":w<CR>:!bash %<CR>","Execute bash script")
 
 -- CD into current file directory
 keymap_loud("n", "<leader>cd", function()
