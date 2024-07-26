21eea50ee9a7dea0642e542e368e906d08375c1f
eb0687 <ebrahim0687@gmail.com>
2024-07-26 14:35:39 +0300

    chore: add highlights

--- lua/eb/utils/lualine-helpers/quickfix.lua
+++ lua/eb/utils/lualine-helpers/quickfix.lua
@@ -20,16 +20,25 @@ end
 -- HELPERS
 
 -- statusline component, showing current and total quickfix item
+-- Define highlight groups
+vim.api.nvim_set_hl(0, "MyHighlightIcon", { bg = "#32302f", fg = "#d3869b" })
+vim.api.nvim_set_hl(0, "MyHighlightCount", { bg = "#32302f", fg = "#a9b665" })
+
+-- Helper function to apply highlight
+local function apply_highlight(group, text)
+    return "%#" .. group .. "#" .. text
+end
+
 function M.counter()
     local totalItems = #vim.fn.getqflist()
     if totalItems == 0 then
         return ""
     end
-    local out = " "
+    local out = apply_highlight("MyHighlightIcon", " ")
     if g.qfCount then
-        out = out .. tostring(g.qfCount) .. "/"
+        out = out .. apply_highlight("MyHighlightCount", tostring(g.qfCount)) .. "/"
     end
-    out = out .. tostring(totalItems)
+    out = out .. apply_highlight("MyHighlightCount", tostring(totalItems))
     return out
 end
 
