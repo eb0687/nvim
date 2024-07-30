48d9322babb1a04b66b892946e4aa07c163a9fe9
eb0687 <ebrahim0687@gmail.com>
2024-07-30 15:34:17 +0300

    fix: fixed db plugins

--- lua/eb/plugins/db.lua
+++ lua/eb/plugins/db.lua
@@ -8,29 +8,30 @@
 -- https://github.com/kristijanhusak/vim-dadbod-completion
 
 return {
-    'kristijanhusak/vim-dadbod-ui',
+    "kristijanhusak/vim-dadbod-ui",
     dependencies = {
-        { 'tpope/vim-dadbod', lazy = true },
+        { "tpope/vim-dadbod", lazy = true },
         {
-            'kristijanhusak/vim-dadbod-completion',
-            ft = { 'sql', 'mysql', 'plsql' },
-            lazy = true
+            "kristijanhusak/vim-dadbod-completion",
+            ft = { "sql", "mysql", "plsql" },
+            lazy = true,
         },
     },
     cmd = {
-        'DBUI',
-        'DBUIToggle',
-        'DBUIAddConnection',
-        'DBUIFindBuffer',
+        "DBUI",
+        "DBUIToggle",
+        "DBUIAddConnection",
+        "DBUIFindBuffer",
     },
     config = function()
-        require('config.dadbod').setup()
-        require('cmp').setup.buffer({
+        -- require("vim-dadbod").setup()
+        require("cmp").setup.buffer({
             vim.cmd(
-            [[autocmd FileType sql,mysql,plsql lua require('cmp').setup.buffer({ sources = {{ name = 'vim-dadbod-completion' }} })]]),
+                [[autocmd FileType sql,mysql,plsql lua require('cmp').setup.buffer({ sources = {{ name = 'vim-dadbod-completion' }} })]]
+            ),
             sources = { {
-                name = 'vim-dadbod-completion'
-            } }
+                name = "vim-dadbod-completion",
+            } },
         })
         vim.g.db_ui_use_nerd_fonts = 1
     end,
