78d625ec48ac5006a074729d389ef0f1ade024c9
eb0687 <ebrahim0687@gmail.com>
2024-07-31 23:26:36 +0300

    feat: add a custom zen mode

--- after/plugin/toggle_alacritty_opacity.lua
+++ after/plugin/toggle_alacritty_opacity.lua
@@ -12,6 +12,20 @@ local function ToggleAlacrittyOpacity()
     end
 end
 
+local function ToggleZen()
+    local home = os.getenv("HOME")
+    local binary_path = home .. "/.local/bin/toggle-alacritty-opacity"
+
+    if vim.fn.executable(binary_path) == 1 then
+        vim.api.nvim_command("!" .. binary_path)
+        vim.api.nvim_command(":" .. "NoNeckPain")
+    else
+        print(binary_path .. " not found")
+    end
+end
+
 user_command("ToggleAlacrittyOpacity", ToggleAlacrittyOpacity, { desc = "Toggle Alacritty Opacity" })
+user_command("ToggleZen", ToggleZen, { desc = "Toggle Custom Zen Mode" })
 
 keymap_normal("<leader>ta", ":ToggleAlacrittyOpacity<CR>", "CUSTOM", true, "Runs the toggle-alacritty-opacity binary")
+keymap_normal("<leader>tz", ":ToggleZen<CR>", "CUSTOM", true, "Toggle custom zen mode")
