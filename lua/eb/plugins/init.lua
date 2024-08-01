f0dfe0c4773f14aa08c7f6b53f101dbd1ffdf49f
eb0687 <ebrahim0687@gmail.com>
2024-08-01 14:29:38 +0300

    feat: add wtf plugin
    quickly search diagnostic errors in a broser + openai integration

--- lua/eb/plugins/init.lua
+++ lua/eb/plugins/init.lua
@@ -188,4 +188,32 @@ return {
             },
         },
     },
+    -- https://github.com/piersolenski/wtf.nvim
+    {
+        "piersolenski/wtf.nvim",
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+        },
+        opts = {
+            search_engine = "google",
+        },
+        keys = {
+            {
+                "gW",
+                mode = { "n", "x" },
+                function()
+                    require("wtf").ai()
+                end,
+                desc = "Debug diagnostic with AI",
+            },
+            {
+                mode = { "n" },
+                "gw",
+                function()
+                    require("wtf").search()
+                end,
+                desc = "Search diagnostic with Google",
+            },
+        },
+    },
 }
