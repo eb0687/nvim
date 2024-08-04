71b7f5a82792540ddda055ad8f97ace55a52f4fe
eb0687 <ebrahim0687@gmail.com>
2024-08-04 12:01:18 +0300

    chore: add some markdown specific linting opts

--- lua/eb/plugins/nvim-lint.lua
+++ lua/eb/plugins/nvim-lint.lua
@@ -16,6 +16,17 @@ return {
         }
         local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
+        local ns = require("lint").get_namespace("markdownlint")
+        vim.diagnostic.config({
+            virtual_text = false,
+            float = {
+                show_header = true,
+                source = "always",
+                border = "rounded",
+                focusable = true,
+            },
+        }, ns)
+
         vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
             group = lint_augroup,
             callback = function()
