7c9b14e21583bfbe42a52035c85c5c878c33a27e
eb0687 <ebrahim0687@gmail.com>
2024-08-05 10:30:00 +0300

    feat: add previewers and changed theme

--- lua/eb/plugins/easypick.lua
+++ lua/eb/plugins/easypick.lua
@@ -16,31 +16,34 @@ return {
                     name = "Dotbare",
                     command = "dotbare ls-files",
                     previewer = easypick.previewers.default(),
+                    opts = require("telescope.themes").get_ivy({}),
                 },
                 {
                     -- this will only work if not inside an existing git repo
                     name = "Dotbare unstagged files",
                     command = "dotbare diff --name-only",
-                    opts = require("telescope.themes").get_dropdown({}),
+                    previewer = easypick.previewers.file_diff(),
+                    opts = require("telescope.themes").get_ivy({}),
                 },
                 {
                     -- Show staged files ready to be commited only
                     name = "Dotbare staged files",
                     command = "dotbare diff --name-only --staged",
-                    opts = require("telescope.themes").get_dropdown({}),
+                    opts = require("telescope.themes").get_ivy({}),
                 },
                 {
                     -- Show staged files ready to be commited only
                     name = "Staged files",
                     command = "git diff --name-only --staged",
                     action = easypick.actions.nvim_commandf("Git commit %s"),
-                    opts = require("telescope.themes").get_dropdown({}),
+                    opts = require("telescope.themes").get_ivy({}),
                 },
                 {
                     -- Show unstagged files only
                     name = "Unstagged files",
                     command = "git diff --name-only",
-                    opts = require("telescope.themes").get_dropdown({}),
+                    previewer = easypick.previewers.file_diff(),
+                    opts = require("telescope.themes").get_ivy({}),
                 },
             },
         })
