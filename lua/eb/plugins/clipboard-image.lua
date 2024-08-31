8352658a89c7b727569412c4f532214b343825bd
eb0687 <ebrahim0687@gmail.com>
2024-08-31 18:56:14 +0300

    feat: add clipboard-image plugin

---
+++ lua/eb/plugins/clipboard-image.lua
@@ -0,0 +1,25 @@
+-- clipboard.nvim
+-- https://github.com/ekickx/clipboard-image.nvim
+
+-- NOTE: fix for the plugin to work
+-- https://github.com/ekickx/clipboard-image.nvim/pull/48
+
+return {
+    "ekickx/clipboard-image.nvim",
+    ft = "markdown",
+    config = function()
+        local ci = require("clipboard-image")
+        ci.setup({
+            default = {
+                img_dir = { "%:p:h", "./assets/img" },
+                img_dir_txt = "assets/img",
+                img_name = function()
+                    vim.fn.inputsave()
+                    local name = vim.fn.input("Name: ")
+                    vim.fn.inputrestore()
+                    return name:gsub(" ", "-")
+                end,
+            },
+        })
+    end,
+}
