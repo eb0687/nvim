70f91d2a773f33f9c53fc5641e406c96902b2907
eb0687 <ebrahim0687@gmail.com>
2024-08-31 19:56:22 +0300

    feat: a few changes
    - update require("silicon") to require("nvim-silicon")
    - copy to clipboard is true
    - save to a specific directory

--- lua/eb/plugins/silicon.lua
+++ lua/eb/plugins/silicon.lua
@@ -11,15 +11,27 @@ return {
     lazy = true,
     cmd = "Silicon",
     config = function()
-        local silicon = require("silicon")
+        local silicon = require("nvim-silicon")
         local function get_title()
             return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), ":t")
+        end
+
+        local custom_output = function()
+            local img_dir = vim.fn.expand("~/Pictures/Screenshots/silicon-screenshots")
+            vim.fn.mkdir(img_dir, "p")
+
+            local timestamp = os.date("!%Y-%m-%dT%H-%M-%SZ")
+            local filename = timestamp .. "_code.png"
+            local new_path = img_dir .. "/" .. filename
+            return new_path
         end
 
         silicon.setup({
             font = "JetBrainsMono Nerd Font Mono=34;Noto Color Emoji=34",
             background = "#7daea3",
             window_title = get_title,
+            to_clipboard = true,
+            output = custom_output,
         })
     end,
 }
