c816bf7d380ba7db69db05cb0039818dd59a573f
eb0687 <ebrahim0687@gmail.com>
2024-12-10 10:32:39 +0300

    chore: update smear opts

--- lua/eb/plugins/init.lua
+++ lua/eb/plugins/init.lua
@@ -232,6 +232,12 @@ return {
     },
     {
         "sphamba/smear-cursor.nvim",
-        opts = {},
+        opts = {
+            legacy_computing_symbols_support = false,
+            stiffness = 0.8, -- 0.6      [0, 1]
+            trailing_stiffness = 0.5, -- 0.3      [0, 1]
+            distance_stop_animating = 0.5, -- 0.1      > 0
+            hide_target_hack = false, -- true     boolean
+        },
     },
 }
