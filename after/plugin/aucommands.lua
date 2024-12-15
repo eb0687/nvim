3f821a75f4013ffaa9390ef7d0209d5bc4dece10
eb0687 <ebrahim0687@gmail.com>
2024-12-15 19:12:48 +0300

    chore: injections

--- after/plugin/aucommands.lua
+++ after/plugin/aucommands.lua
@@ -102,3 +102,32 @@ vim.api.nvim_create_user_command("FindAndReplaceGlobal", function(opts)
     vim.api.nvim_command(string.format("cfdo s/%s/%s/g | update | bd", opts.fargs[1], opts.fargs[2]))
     utils.clear_quickfix()
 end, { nargs = "*" })
+
+-- source: https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#command-to-toggle-format-on-save
+require("conform").setup({
+    format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+            return
+        end
+        return { timeout_ms = 500, lsp_format = "fallback" }
+    end,
+})
+
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+    if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
+    else
+        vim.g.disable_autoformat = true
+    end
+end, {
+    desc = "Disable autoformat-on-save",
+    bang = true,
+})
+vim.api.nvim_create_user_command("FormatEnable", function()
+    vim.b.disable_autoformat = false
+    vim.g.disable_autoformat = false
+end, {
+    desc = "Re-enable autoformat-on-save",
+})
--- lua/eb/plugins/formatting.lua
+++ lua/eb/plugins/formatting.lua
@@ -19,21 +19,27 @@ return {
                 python = { "black" },
                 go = { "goimports", "gofumpt", "golines" },
                 bash = { "beautysh", "shellharden" },
+                sql = { "sql_formatter" },
+                ["*"] = { "injected" },
             },
             format_on_save = {
-                lsp_fallback = true,
+                lsp_fallback = "always",
                 async = false,
                 timeout_ms = 1000,
             },
         })
 
+        -- require("conform").formatters.sql_formatter = {
+        --     prepend_args = { "-c", vim.fn.expand("~/.config/sql_formatter.json") },
+        -- }
+
         -- BUG: The following keymap does not work
-        vim.keymap.set({ "n", "v" }, "<leader>fr", function()
-            conform.format({
-                lsp_fallback = true,
-                async = false,
-                timeout_ms = 1000,
-            })
-        end, { desc = "Format file" })
+        -- vim.keymap.set({ "n", "v" }, "<leader>fr", function()
+        --     conform.format({
+        --         lsp_fallback = true,
+        --         async = false,
+        --         timeout_ms = 1000,
+        --     })
+        -- end, { desc = "Format file" })
     end,
 }
