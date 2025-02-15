77c727239dfc3ce9eb1ed1480682a1d0c0a61fdc
eb0687 <ebrahim0687@gmail.com>
2025-02-15 17:13:28 +0300

    chore: update git-conflict highlights

--- lua/eb/plugins/git-conflict.lua
+++ lua/eb/plugins/git-conflict.lua
@@ -2,18 +2,18 @@ return {
     "akinsho/git-conflict.nvim",
     version = "*",
     config = function()
-        vim.api.nvim_set_hl(0, "DiffAddCustom", { fg = "#7daea3", bg = "#404946" })
-        vim.api.nvim_set_hl(0, "DiffTextCustom", { fg = "#7daea3", bg = "#542937" })
+        vim.api.nvim_set_hl(0, "CurrentCustom", { fg = "#7daea3", bg = "#404946" })
+        vim.api.nvim_set_hl(0, "IncomingCustom", { fg = "#7daea3", bg = "#542937" })
 
         local gc = require("git-conflict")
         gc.setup({
-            default_mappings = true, -- disable buffer local mapping created by this plugin
-            default_commands = true, -- disable commands created by this plugin
-            disable_diagnostics = true, -- This will disable the diagnostics in a buffer whilst it is conflicted
-            list_opener = "copen", -- command or function to open the conflicts list
-            highlights = { -- They must have background color, otherwise the default color will be used
-                incoming = "DiffAddCustom",
-                current = "DiffTextCustom",
+            default_mappings = true,
+            default_commands = true,
+            disable_diagnostics = true,
+            list_opener = "copen",
+            highlights = {
+                incoming = "IncomingCustom",
+                current = "CurrentCustom",
             },
         })
     end,
