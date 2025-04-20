e9fed3b86e79ff5aee0a3c93485e02edc0ed09b3
eb0687 <ebrahim0687@gmail.com>
2025-04-20 12:18:38 +0300

    chore: make scratch appear on the right

--- after/plugin/scratch.lua
+++ after/plugin/scratch.lua
@@ -1,7 +1,8 @@
 -- source: https://github.com/MariaSolOs/dotfiles/blob/62284b49d5a3428f33c3622d6b2a0a9590931c80/.config/nvim/lua/commands.lua
 -- NOTE: open a scratch buffer
 vim.api.nvim_create_user_command("Scratch", function()
-    vim.cmd("bel 10new")
+    -- vim.cmd("bel 10new")
+    vim.cmd("rightbelow vnew")
     local buf = vim.api.nvim_get_current_buf()
     for name, value in pairs({
         filetype = "scratch",
