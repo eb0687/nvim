d61175efa38e88013ca0863d912429601734112c
eb0687 <ebrahim0687@gmail.com>
2025-04-23 21:27:34 +0300

    chore: refactor to use opts

--- lua/eb/plugins/auto-sessions.lua
+++ lua/eb/plugins/auto-sessions.lua
@@ -3,35 +3,38 @@
 
 return {
     "rmagatti/auto-session",
+    lazy = false,
     dependencies = {
         "nvim-telescope/telescope.nvim", -- :Telescope session-lens
     },
-    config = function()
-        local auto_session = require("auto-session")
-        local custom_helpers = require("eb.utils.custom_helpers")
-        local keymap_normal = custom_helpers.keymap_normal
 
-        auto_session.setup({
-            auto_create = true,
-            auto_save = true,
-            auto_restore = false,
-            suppressed_dirs = {
-                "~/Downloads",
-                "~/Documents",
-            },
-        })
-        keymap_normal("<leader>wr", "<cmd>SessionRestore<CR>", "AUTO SESSION", true, "Restore session for cwd")
-        keymap_normal(
+    ---@module "auto-session"
+    ---@type AutoSession.Config
+    opts = {
+        auto_create = true,
+        auto_save = true,
+        auto_restore = false,
+        suppressed_dirs = {
+            "~/Downloads",
+            "~/Documents",
+        },
+    },
+
+    keys = {
+        {
+            "<leader>wr",
+            "<cmd>SessionRestore<CR>",
+            desc = "AUTO SESSION: Restore session for cwd",
+        },
+        {
             "<leader>ws",
             "<cmd>SessionSave<CR>",
-            "AUTO SESSION",
-            true,
-            "Save session for auto session root dir"
-        ) -- save workspace session for current working directory
-        keymap_normal(
+            desc = "AUTO SESSION: Save session for auto session root dir",
+        },
+        {
             "<leader>wl",
-            require("auto-session.session-lens").search_session,
-            "List all sessions in Telescope"
-        )
-    end,
+            "<cmd>Telescope session-lens<CR>",
+            desc = "AUTO SESSION: List all sessions in Telescope",
+        },
+    },
 }
