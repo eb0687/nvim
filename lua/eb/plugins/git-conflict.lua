02e071dad4b7701e54395bbc4eed19442d45fdea
eb0687 <ebrahim0687@gmail.com>
2025-04-24 10:18:16 +0300

    refactor: using opts

--- lua/eb/plugins/git-conflict.lua
+++ lua/eb/plugins/git-conflict.lua
@@ -1,21 +1,28 @@
 return {
     "akinsho/git-conflict.nvim",
     version = "*",
-    config = function()
+
+    ---@module "git-conflict"
+    ---@type GitConflictUserConfig
+    opts = {
+        default_mappings = false,
+        default_commands = true,
+        disable_diagnostics = true,
+        list_opener = "copen",
+        highlights = {
+            incoming = "IncomingCustom",
+            current = "CurrentCustom",
+        },
+        debug = false,
+    },
+
+    config = function(_, opts)
+        -- custom highlights
         vim.api.nvim_set_hl(0, "CurrentCustom", { fg = "#7daea3", bg = "#404946" })
         vim.api.nvim_set_hl(0, "IncomingCustom", { fg = "#7daea3", bg = "#542937" })
 
         local gc = require("git-conflict")
-        gc.setup({
-            default_mappings = false,
-            default_commands = true,
-            disable_diagnostics = true,
-            list_opener = "copen",
-            highlights = {
-                incoming = "IncomingCustom",
-                current = "CurrentCustom",
-            },
-        })
+        gc.setup(opts)
 
         local function choose_ours()
             vim.cmd("GitConflictChooseOurs")
