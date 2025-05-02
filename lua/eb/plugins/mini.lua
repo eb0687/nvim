6fd8993204fa93d5af4762e0e01317e67756b426
eb0687 <ebrahim0687@gmail.com>
2025-05-02 12:56:01 +0300

    chore: update mini keymaps

--- lua/eb/plugins/mini.lua
+++ lua/eb/plugins/mini.lua
@@ -24,7 +24,21 @@ return {
 
         -- source: https://github.com/echasnovski/mini.surround
         local surround = require("mini.surround")
-        surround.setup({ highlight_duration = 200 })
+        surround.setup({
+            highlight_duration = 200,
+            mappings = {
+                add = "gsa", -- Add surrounding in Normal and Visual modes
+                delete = "gsd", -- Delete surrounding
+                find = "gsf", -- Find surrounding (to the right)
+                find_left = "gsF", -- Find surrounding (to the left)
+                highlight = "gsh", -- Highlight surrounding
+                replace = "gsr", -- Replace surrounding
+                update_n_lines = "gsn", -- Update `n_lines`
+
+                suffix_last = "l", -- Suffix to search with "prev" method
+                suffix_next = "n", -- Suffix to search with "next" method
+            },
+        })
 
         -- source: https://github.com/echasnovski/mini.pairs
         local pairs = require("mini.pairs")
