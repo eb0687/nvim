cbb18524acf462d89e794fe09332995bfc57ccc6
eb0687 <ebrahim0687@gmail.com>
2025-08-07 13:14:49 +0300

    chore: cleanup

--- lua/eb/plugins/mini.lua
+++ lua/eb/plugins/mini.lua
@@ -231,23 +231,6 @@ return {
         pick.setup({
             mappings = {
                 choose_marked = "<C-q>",
-                -- execute = {
-                --     char = "<C-e>",
-                --     func = function()
-                --         local match = MiniPick.get_picker_matches()
-                --         if not match then
-                --             return
-                --         end
-                --         local value = type(match.current) == "table" and match.current.value or tostring(match.current)
-                --         value = value:gsub("^:+", "")
-                --         MiniPick.stop()
-                --         vim.schedule(function()
-                --             local cmd = ":" .. value
-                --             local keys = vim.api.nvim_replace_termcodes(cmd, true, false, true)
-                --             vim.api.nvim_feedkeys(keys, "n", false)
-                --         end)
-                --     end,
-                -- },
             },
             options = {
                 content_from_bottom = true,
@@ -283,6 +266,7 @@ return {
             end)
         end
 
+        -- SOURCE: https://github.com/echasnovski/mini.nvim/discussions/1926
         pick.registry.command_history = function()
             local edit_command = function()
                 local match = MiniPick.get_picker_matches()
