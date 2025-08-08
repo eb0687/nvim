936ef3c83fc34f0e3a315e05dfdd1a9f609fe277
eb0687 <ebrahim0687@gmail.com>
2025-08-08 20:23:33 +0300

    chore: update mini hipatterns

--- lua/eb/plugins/mini.lua
+++ lua/eb/plugins/mini.lua
@@ -160,14 +160,18 @@ return {
         -- NOTE: look at the examples for custom setup similar to todo-comments
         ----------------------------
         local hipatterns = require("mini.hipatterns")
-        local hi_words = extra.gen_highlighter.words
+        -- local hi_words = extra.gen_highlighter.words
         hipatterns.setup({
             highlighters = {
-                todo = { pattern = "%f[%w]()TODO.*", group = "MiniHipatternsTodo" },
-                fixme = { pattern = "%f[%w]()FIXME.*", group = "MiniHipatternsFixme" },
-                test = { pattern = "%f[%w]()TEST.*", group = "MiniHipatternsHack" },
-                note = hi_words({ "NOTE", "INFO" }, "MiniHipatternsNote"),
-                source = hi_words({ "SOURCE", "source" }, "MiniHipatternsNote"),
+                todo = { pattern = "%f[%w]()TODO:.*", group = "MiniHipatternsTodo" },
+                fixme = { pattern = "%f[%w]()FIXME:.*", group = "MiniHipatternsFixme" },
+                test = { pattern = "%f[%w]()TEST:.*", group = "MiniHipatternsHack" },
+                note = { pattern = "%f[%w]()NOTE:.*", group = "MiniHipatternsNote" },
+                info = { pattern = "%f[%w]()INFO:.*", group = "MiniHipatternsNote" },
+                source = { pattern = "%f[%w]()SOURCE:.*", group = "MiniHipatternsNote" },
+                small_source = { pattern = "%f[%w]()source:.*", group = "MiniHipatternsNote" },
+                -- note = hi_words({ "NOTE", "INFO" }, "MiniHipatternsNote"),
+                -- source = hi_words({ "SOURCE", "source" }, "MiniHipatternsNote"),
 
                 hex_color = hipatterns.gen_highlighter.hex_color(),
             },
