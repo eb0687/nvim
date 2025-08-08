91e0bff9fd691c7ec3dbfb6f840f307211caf2e1
eb0687 <ebrahim0687@gmail.com>
2025-08-08 10:17:54 +0300

    chore: optimize lazydev?

--- lua/eb/plugins/lazydev.lua
+++ lua/eb/plugins/lazydev.lua
@@ -5,7 +5,7 @@ return {
         library = {
             { path = "${3rd}/luv/library", words = { "vim%.uv" } },
             -- always load the LazyVim library
-            "LazyVim",
+            -- "LazyVim",
             -- Only load the lazyvim library when the `LazyVim` global is found
             { path = "LazyVim", words = { "LazyVim" } },
         },
