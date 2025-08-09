641952754bb02eca38b8ff4904bd67006360eea4
eb0687 <ebrahim0687@gmail.com>
2025-08-09 17:29:57 +0300

    chore: cleanup

--- after/ftplugin/json.lua
+++ after/ftplugin/json.lua
@@ -4,8 +4,8 @@ set.tabstop = 2
 set.shiftwidth = 2
 
 -- SOURCE: https://github.com/JoosepAlviste/dotfiles/blob/c171efbbbe0daa5e737250ec82338a51ed53c15a/config/nvim/ftplugin/json.lua
--- When creating a new line with o, make sure there is a trailing comma on the
--- current line
+-- SOURCE: https://medium.com/scoro-engineering/5-smart-mini-snippets-for-making-text-editing-more-fun-in-neovim-b55ffb96325a
+-- When creating a new line with o, make sure there is a trailing comma on the current line
 vim.keymap.set("n", "o", function()
     local line = vim.api.nvim_get_current_line()
 
