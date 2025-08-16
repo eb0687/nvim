e5312f1ba951f49d5dcd071c8f3b3698cfeb551c
eb0687 <ebrahim0687@gmail.com>
2025-08-16 14:07:50 +0300

    chore: cleanup

--- lua/eb/init.lua
+++ lua/eb/init.lua
@@ -7,7 +7,6 @@ require("eb.utils.reload")
 require("eb.utils.rotate_window")
 require("eb.utils.copy_file_path_to_clipboard")
 require("eb.utils.clear_quickfixlist")
--- require("eb.godot-setup")
 -- require("eb.utils.vertical_help")
 
 -- TEST:
