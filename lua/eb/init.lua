3eb6050217a2423abf5905fb1177d1f22db0ff61
eb0687 <ebrahim0687@gmail.com>
2025-10-05 13:05:36 +0300

    chore: cleanup

--- lua/eb/init.lua
+++ lua/eb/init.lua
@@ -8,7 +8,3 @@ require("eb.utils.rotate_window")
 require("eb.utils.copy_file_path_to_clipboard")
 require("eb.utils.clear_quickfixlist")
 require("eb.tools.sql-runner")
--- require("eb.utils.vertical_help")
-
--- TEST:
--- print("hello from EB")
