eec7d90d5fcd59c90bb231e97de897d3ec685ee4
eb0687 <ebrahim0687@gmail.com>
2025-11-02 22:28:42 +0300

    feat: add qmk plugin

---
+++ lua/eb/plugins/jiga/qmk.lua
@@ -0,0 +1,16 @@
+return {
+    "codethread/qmk.nvim",
+    filetype = "keymap",
+    enabled = true,
+    event = "VeryLazy",
+    opts = {
+        name = "corne",
+        variant = "zmk",
+        layout = {
+            "x x x x x x _ x x x x x x",
+            "x x x x x x _ x x x x x x",
+            "x x x x x x _ x x x x x x",
+            "_ _ _ x x x _ x x x _ _ _",
+        },
+    },
+}
