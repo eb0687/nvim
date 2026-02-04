8b9ecdb817c5899637230ace095625bb1654abdb
eb0687 <ebrahim0687@gmail.com>
2026-02-04 12:39:59 +0300

    chore: update location color in statusline

--- lua/eb/utils/mini-helpers/colors.lua
+++ lua/eb/utils/mini-helpers/colors.lua
@@ -44,6 +44,6 @@ return {
     git = { fg = colors.color5, bg = colors.bg },
     macro = { fg = colors.color9, bg = colors.bg },
     lsp = { fg = colors.color5, bg = colors.bg },
-    location = { fg = colors.color2, bg = colors.bg },
+    location = { fg = colors.color4, bg = colors.bg },
     lazy = { fg = colors.color6, bg = colors.bg },
 }
