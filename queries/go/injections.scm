d77755f5f7de983a0e2ba5cd25bf8b25dbeda723
eb0687 <ebrahim0687@gmail.com>
2026-02-11 11:48:53 +0300

    chore: remove queries

--- queries/go/injections.scm
+++
@@ -1,7 +0,0 @@
-; extends
-
-; SOURCE: https://github.com/tree-sitter/tree-sitter/discussions/1577
-((
-  (raw_string_literal_content) @constant
-  (#match? @constant "(SELECT|select|insert|INSERT|UPDATE|update|DELETE|delete).*")
-)@injection.content (#set! injection.language "sql"))
