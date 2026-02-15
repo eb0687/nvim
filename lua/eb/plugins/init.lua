6310cb34b6151de49b595f06eec99fab23bb9927
eb0687 <ebrahim0687@gmail.com>
2026-02-15 18:44:39 +0300

    chore: lazy-lock and disable better-escape

--- lua/eb/plugins/init.lua
+++ lua/eb/plugins/init.lua
@@ -87,6 +87,7 @@ return {
     },
     {
         "max397574/better-escape.nvim",
+        enabled = false,
         config = function()
             require("better_escape").setup({
                 mappings = {
