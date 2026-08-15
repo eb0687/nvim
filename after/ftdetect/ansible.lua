cfb01f2f6fb986d25c49135c0fd5e92b8e13d59f
eb0687 <ebrahim0687@gmail.com>
2026-08-15 22:13:55 +0300

    chore: add meta, inventory, and requirements filetype detection for ansible

--- after/ftdetect/ansible.lua
+++ after/ftdetect/ansible.lua
@@ -4,6 +4,9 @@ vim.filetype.add({
         [".*/roles/[^/]+/handlers/.*%.ya?ml"] = "yaml.ansible",
         [".*/roles/[^/]+/defaults/.*%.ya?ml"] = "yaml.ansible",
         [".*/roles/[^/]+/vars/.*%.ya?ml"] = "yaml.ansible",
+        [".*/roles/[^/]+/meta/.*%.ya?ml"] = "yaml.ansible",
         [".*/playbooks/.*%.ya?ml"] = "yaml.ansible",
+        [".*/inventory/.*%.ya?ml"] = "yaml.ansible",
+        [".*/requirements%.ya?ml"] = "yaml.ansible",
     },
 })
