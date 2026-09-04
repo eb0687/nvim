0a628055ad66310862f648461e6b97811fc39594
eb0687 <ebrahim0687@gmail.com>
2026-09-04 11:48:02 +0300

    chore: cleanup

--- lua/eb/init.lua
+++ lua/eb/init.lua
@@ -3,7 +3,6 @@ require("eb.keymaps")
 require("eb.lazy")
 
 -- custom commands
-require("eb.utils.reload")
 require("eb.utils.copy_file_path_to_clipboard")
 require("eb.utils.clear_quickfixlist")
 require("eb.tools.sql-runner")
